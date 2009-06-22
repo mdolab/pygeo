68ab913e099cea4dc568a4dff97f84a27dabb1e4
kenway <kenway@localhost>
2009-06-22 04:20:27 +0000

    update to pyGeo

--- pyGeo.py
+++ pyGeo.py
@@ -51,7 +51,7 @@ class pyGeo():
     Geo object class
     '''
 
-    def __init__(self,L,ref_axis,le_loc,chord,twist,rot_x,rot_y,af_list,N=10):
+    def __init__(self,ref_axis,le_loc,chord,twist,rot_x,rot_y,af_list,N=15):
 
         
         '''Create an instance of the geometry object. Input is through simple
@@ -60,9 +60,6 @@ class pyGeo():
         
         Input: 
         
-        L, scalar : Characteristic length of surface. Span for wings, 
-        blade length for wind turbines
-
         ref_axis, array, size(naf,3): List of points in space defining the 
         the reference  axis for the geometry. Units are consistent with the
         remainder of the geometry. For wind turbine blades, this
@@ -92,9 +89,15 @@ class pyGeo():
         input to pySpline'''
 
         # Save the data to the class
-        self.L        = L
+        assert (len(ref_axis)==len(le_loc) == len(chord) \
+            == len(twist) == len(af_list)),\
+            "All the input data must contain the same number of records"
+
+        naf = len(chord)
+        self.naf = naf
         self.ref_axis = ref_axis
-        self.sloc     = ref_axis[:,2]/L
+        self.ref_axis_refernece = copy.deepcopy(ref_axis)
+     
         self.le_loc   = le_loc
         self.chord    = chord
         self.twist    = twist
@@ -102,100 +105,32 @@ class pyGeo():
         self.rot_y    = rot_y
         self.af_list  = af_list
         self.N        = N
+        self.DVlist   = {}
+        self.sloc = zeros(naf)
+        
 
-        assert (len(ref_axis)==len(le_loc) == len(chord) \
-            == len(twist) == len(af_list)),\
-            "All the input data must contain the same number of records"
-
-        naf = len(chord)
-        self.naf = naf
-        # N is the number of points on each surface
-
-        # This section reads in the coordinates, finds the te and reorders
-        # the points so that it starts at the upper surface of the te 
-        # goes around the le and ends at the lower surface te.
-
+        for i in xrange(naf-1):
+            self.sloc[i+1] = self.sloc[i] +sqrt( (ref_axis[i+1,0]-ref_axis[i,0])**2 + \
+                                                 (ref_axis[i+1,1]-ref_axis[i,1])**2 +
+                                                 (ref_axis[i+1,2]-ref_axis[i,2])**2  )
+        #Normalize
+        self.sloc/=self.sloc[-1]
+      
         # This is the standard cosine distribution in x (one sided)
         s_interp = 0.5*(1-cos(linspace(0,pi,N)))
-        #s_interp[1] = .001
-        #s_interp = linspace(0,1,N)**3
-
         self.s = s_interp
         
         X = zeros([2,N,naf,3])
-
-#        surf_u = zeros((naf,N,3)) #Mast list of points (upper)
-#        surf_l = zeros((naf,N,3)) #Mast list of points (lower)
-
-        n_nodes = zeros((naf,2),int) #number of nodes read in 
-        
         for i in xrange(naf):
-            n_temp,temp_x,temp_y = self.__load_af(af_list[i])
-            
-            # -------------
-            # Upper Surfce
-            # -------------
 
-            # Find the trailing edge point
-            index = where(temp_x == 1)
-            te_index = index[0][0]
-            n_nodes[i,0] = te_index+1   # number of nodes on upper surface
-            n_nodes[i,1] = int(n_temp-te_index) # nodes on lower surface
-    
-            # upper Surface Nodes
-            x_u = temp_x[0:n_nodes[i,0]]
-            y_u = temp_y[0:n_nodes[i,0]]
-            
-            # Now determine the upper surface 's' parameter
+            X_u,Y_u,X_l,Y_l = self.__load_af(af_list[i],N)
 
-            s = zeros(n_nodes[i,0])
-            for j in xrange(n_nodes[i,0]-1):
-                s[j+1] = s[j] + sqrt((x_u[j+1]-x_u[j])**2 + (y_u[j+1]-y_u[j])**2)
-            # end for
-            s = s/s[-1] #Normalize s
-
-            # linearly interpolate to find the points at the positions we want
-            x_interp_u = numpy.interp(s_interp,s,x_u)
-            y_interp_u = numpy.interp(s_interp,s,y_u)
-    
-            
-            # -------------
-            # Lower Surface
-            # -------------
-            n_nodes[i,0] = te_index+1   # number of nodes on upper surface
-            n_nodes[i,1] = n_temp-te_index+1 # number of nodes on lower surface
-            
-            x_l = temp_x[te_index:n_temp]
-            y_l = temp_y[te_index:n_temp]
-            x_l = hstack([x_l,0])
-            y_l = hstack([y_l,0])
-
-            # Reverse coordinates for lower spline
-            #x_l = x_l[::-1]
-            #y_l = y_l[::-1]
-
-            # Now determine the lower surface 's' parameter
-
-            s = zeros(n_nodes[i,1])
-            for j in xrange(n_nodes[i,1]-1):
-                s[j+1] = s[j] + sqrt((x_l[j+1]-x_l[j])**2 + (y_l[j+1]-y_l[j])**2)
-            # end for
-            s = s/s[-1] #Normalize s
-
-            # linearly interpolate to find the points at the positions we want
-            x_interp_l = numpy.interp(s_interp,s,x_l)
-            y_interp_l = numpy.interp(s_interp,s,y_l)
-
-            # --------------------------
-            # Final Coordinate Positions
-            # --------------------------
-
-            X[0,:,i,0] = (x_interp_u-le_loc[i])*chord[i]
-            X[0,:,i,1] = y_interp_u*chord[i]
+            X[0,:,i,0] = (X_u-le_loc[i])*chord[i]
+            X[0,:,i,1] = Y_u*chord[i]
             X[0,:,i,2] = 0
             
-            X[1,:,i,0] = (x_interp_l-le_loc[i])*chord[i]
-            X[1,:,i,1] = y_interp_l*chord[i]
+            X[1,:,i,0] = (X_l-le_loc[i])*chord[i]
+            X[1,:,i,1] = Y_l*chord[i]
             X[1,:,i,2] = 0
             
             for j in xrange(N):
@@ -204,208 +139,107 @@ class pyGeo():
                     X[isurf,j,i,:] = self.__rotx(X[isurf,j,i,:],rot_x[i]*pi/180) # Dihediral Rotation
                     X[isurf,j,i,:] = self.__roty(X[isurf,j,i,:],rot_y[i]*pi/180) # Sweep Rotation
             #end for
-            X[:,:,i,:] += ref_axis[i,:]
+           
             # Finally translate according to axis:
+            X[:,:,i,:] += ref_axis[i,:]
         #end for
         
         self.X = X
-        
         return
-
-
-
-#     def __init__(self,L,ref_axis,le_loc,chord,twist,rot_x,rot_y,af_list,N=10):
-
-        
-#         '''Create an instance of the geometry object. Input is through simple
-#         arrays. Most error checking is left to the user making an instance of
-#         this class. 
-        
-#         Input: 
-        
-#         L, scalar : Characteristic length of surface. Span for wings, 
-#         blade length for wind turbines
-
-#         ref_axis, array, size(naf,3): List of points in space defining the 
-#         the reference  axis for the geometry. Units are consistent with the
-#         remainder of the geometry. For wind turbine blades, this
-#         will be the pitch axis. For aircraft this will usually be the 1/4 
-#         chord. The twist is defined about this axis. 
-
-#         le_loc, array, size(naf) : Location of the LE point ahead of the 
-#         reference axis. Typically this will be 1/4. United scaled by chord
-
-#         chord, array, size(naf) : Chord lengths in consistent units
-        
-#         twist, array, size(naf) : Twist of each section about the reference
-#         axis. Given in units of deg. 
-
-#         af_list, list, size(naf) : List of the filenames  to be read for each
-#         cross section.
-
-#         Output:
-
-#         x,y,z, array, size(naf,2N-1) : Coordinates of the surface for input to 
-#         pySpline.
-
-#         u, array, size(naf): parametric (spanwise) u coordinates for 
-#         input to pySpline 
-        
-#         v, array, size(2*N-1): parametric (chordwise) v coordinates for 
-#         input to pySpline'''
-
-#         # Save the data to the class
-#         self.L        = L
-#         self.ref_axis = ref_axis
-#         self.sloc     = ref_axis[:,2]/L
-#         self.le_loc   = le_loc
-#         self.chord    = chord
-#         self.twist    = twist
-#         self.rot_x    = rot_x
-#         self.rot_y    = rot_y
-#         self.af_list  = af_list
-#         self.surface  = None
-#         self.N        = N
-
-#         assert (len(ref_axis)==len(le_loc) == len(chord) \
-#             == len(twist) == len(af_list)),\
-#             "All the input data must contain the same number of records"
-
-#         naf = len(chord)
-#         self.naf = naf
-#         # N is the number of points on each surface
-
-#         # This section reads in the coordinates, finds the te and reorders
-#         # the points so that it starts at the upper surface of the te 
-#         # goes around the le and ends at the lower surface te.
-
-#         # This is the standard cosine distribution in x (one sided)
-#         x_interp = 0.5*(1-cos(linspace(0,pi,N)))
-#         x = zeros((naf,N*2-1,3)) #Mast list of points
-
-#         n_nodes = zeros((naf,2),int) #number of nodes read in 
-        
-#         for i in xrange(naf):
-#             n_temp,temp_x,temp_y = self.__load_af(af_list[i])
-            
-#             # Find the trailing edge point
-#             index = where(temp_x == 1)
-#             te_index = index[0][0]
-#             n_nodes[i,0] = te_index+1   # number of nodes on upper surface
-#             n_nodes[i,1] = int(n_temp-te_index) # nodes on lower surface
-    
-#             # upper Surface
-#             x_u = temp_x[0:n_nodes[i,0]]
-#             y_u = temp_y[0:n_nodes[i,0]]
-            
-#             # linearly interpolate to find the points at the positions we want
-#             y_interp_u = numpy.interp(x_interp,x_u,y_u)
-    
-#             # Reverse upper coordinates to start at te
-#             y_interp_u = y_interp_u[::-1]
-#             x_interp_u = x_interp[::-1]
-    
-#             # lower Surface
-    
-#             n_nodes[i,0] = te_index+1   # number of nodes on upper surface
-#             n_nodes[i,1] = n_temp-te_index # number of nodes on lower surface
-            
-#             x_l = temp_x[te_index:n_temp]
-#             y_l = temp_y[te_index:n_temp]
-
-#             # Reverse coordinates for lower spline
-#             x_l = x_l[::-1]
-#             y_l = y_l[::-1]
-
-#             x_interp_l = x_interp[1:] #DO NOT want 0,0 in lower surface
-
-#             # Interpolate
-#             y_interp_l = numpy.interp(x_interp_l,x_l,y_l)
-
-
-#             x_cor_full = hstack([x_interp_u,x_interp_l])-le_loc[i]
-#             y_cor_full = hstack([y_interp_u,y_interp_l])
-
-#             #Determine Final Coordinate Position
-            
-#             x[i,:,0] = x_cor_full*chord[i]
-#             x[i,:,1] = y_cor_full*chord[i]
-#             x[i,:,2] = 0
-            
-#             for j in xrange(2*N-1):
-#                 x[i,j,:] = self.__rotz(x[i,j,:],twist[i]*pi/180) # Twist Rotation
-#                 x[i,j,:] = self.__rotx(x[i,j,:],rot_x[i]*pi/180) # Dihediral Rotation
-#                 x[i,j,:] = self.__roty(x[i,j,:],rot_y[i]*pi/180) # Sweep Rotation
-#             #end for
-
-#             # Finally translate according to axis:
-
-#             x[i,:,:] += ref_axis[i,:]
-
-#         #end for
-        
-#         self.x = x #The coordinates 
-#         #print x
 
     def createSurface(self):
 
         '''Create the splined surface based on the input geometry'''
 
-        print 'creating surface:'
+        print 'creating surfaces:'
         u = zeros([2,self.N])
         v = zeros([2,self.naf])
 
         u[:] = self.s
         v[:] = (self.ref_axis[:,2])/max(self.ref_axis[:,2])#*2-1
-
-        print 'u:',u
-        print 'v:',v
-        
+               
         print 'creating surfaces...'
-        self.surf1 = pySpline.spline(2,u,v,self.X,fit_type='interpolate')
-        self.surf2 = pySpline.spline(2,u,v,self.X,fit_type='lms',Nctlu=13,Nctlv=9)
-        self.surf1.writeTecplot('output.dat')
-        self.surf2.writeTecplot('output2.dat')
-        sys.exit(0)
+        self.surf = pySpline.spline(2,u,v,self.X,fit_type='interpolate',ku=4,kv=2)
+        #self.surf = pySpline.spline(2,u,v,self.X,fit_type='lms',Nctlu=13,Nctlv=10,ku=4,kv=4)
+       
         return
 
-    def joinSurfaces(self):
-        # self.surf_l.cx[0:self.surf_u.nctlu] = self.surf_u.cx[0:self.surf_u.nctlu]
-#         self.surf_l.cy[0:self.surf_u.nctlu] = self.surf_u.cy[0:self.surf_u.nctlu]
-#         self.surf_l.cz[0:self.surf_u.nctlu] = self.surf_u.cz[0:self.surf_u.nctlu]
-#         self.surf_l.cx[::self.surf_u.nctlv] =   self.surf_u.cx[::self.surf_u.nctlv]
-#         self.surf_l.cy[::self.surf_u.nctlv] =   self.surf_u.cy[::self.surf_u.nctlv]
-#         self.surf_l.cz[::self.surf_u.nctlv] =   self.surf_u.cz[::self.surf_u.nctlv]
-        return
-
-    def __load_af(self,filename):
+    def __load_af(self,filename,N=35):
         ''' Load the airfoil file from precomp format'''
+
+        # Interpolation Format
+        s_interp = 0.5*(1-cos(linspace(0,pi,N)))
+
         f = open(filename,'r')
 
-        new_aux = string.split(f.readline())
-        naf = int(new_aux[0]) 
+        aux = string.split(f.readline())
+        npts = int(aux[0]) 
         
-        xnodes = zeros(naf)
-        ynodes = zeros(naf)
+        xnodes = zeros(npts)
+        ynodes = zeros(npts)
 
         f.readline()
         f.readline()
         f.readline()
 
-        for i in xrange(naf):
-
-            new_aux = string.split(f.readline())
-
-            xnodes[i] = float(new_aux[0])
-            ynodes[i] = float(new_aux[1])
-            
+        for i in xrange(npts):
+            aux = string.split(f.readline())
+            xnodes[i] = float(aux[0])
+            ynodes[i] = float(aux[1])
+        # end for
         f.close()
-        return naf,xnodes, ynodes
+
+        # -------------
+        # Upper Surfce
+        # -------------
+
+        # Find the trailing edge point
+        index = where(xnodes == 1)
+        te_index = index[0]
+        n_upper = te_index+1   # number of nodes on upper surface
+        n_lower = int(npts-te_index)+1 # nodes on lower surface
+
+        # upper Surface Nodes
+        x_u = xnodes[0:n_upper]
+        y_u = ynodes[0:n_upper]
+
+        # Now determine the upper surface 's' parameter
+
+        s = zeros(n_upper)
+        for j in xrange(n_upper-1):
+            s[j+1] = s[j] + sqrt((x_u[j+1]-x_u[j])**2 + (y_u[j+1]-y_u[j])**2)
+        # end for
+        s = s/s[-1] #Normalize s
+
+        # linearly interpolate to find the points at the positions we want
+        X_u = numpy.interp(s_interp,s,x_u)
+        Y_u = numpy.interp(s_interp,s,y_u)
+
+        # -------------
+        # Lower Surface
+        # -------------
+        x_l = xnodes[te_index:npts]
+        y_l = ynodes[te_index:npts]
+        x_l = hstack([x_l,0])
+        y_l = hstack([y_l,0])
+
+        # Now determine the lower surface 's' parameter
+
+        s = zeros(n_lower)
+        for j in xrange(n_lower-1):
+            s[j+1] = s[j] + sqrt((x_l[j+1]-x_l[j])**2 + (y_l[j+1]-y_l[j])**2)
+        # end for
+        s = s/s[-1] #Normalize s
+
+        # linearly interpolate to find the points at the positions we want
+        X_l = numpy.interp(s_interp,s,x_l)
+        Y_l = numpy.interp(s_interp,s,y_l)
+
+        return X_u,Y_u,X_l,Y_l
 
     def __rotx(self,x,theta):
         ''' Rotate a set of airfoil coodinates in the local x frame'''
         M = [[1,0,0],[0,cos(theta),-sin(theta)],[0,sin(theta),cos(theta)]]
-        
         return dot(M,x)
 
     def __roty(self,x,theta):
@@ -463,8 +297,118 @@ class pyGeo():
 
         return x
 
+    def addVar(self,dv_name,value,mapping,lower=0,upper=1):
 
+        '''Add a single (scalar) variable to the dv list. '''
+
+        if dv_name in self.DVlist.keys():
+            print 'Error: dv_name is already in the list of keys. Please use a unique deisgn variable name'
+            sys.exit(0)
+        # end if
+        
+        self.DVlist[dv_name] = geoDV(dv_name,value,mapping,lower,upper)
+        
+        
+        return
     
+    def updateDV(self):
+        '''Update the B-spline control points from the Design Varibales'''
+        print 'self.sloc',self.sloc
+        for key in self.DVlist.keys():
+            self.DVlist[key].applyValue(self.surf,self.sloc)
+
+
+class geoDV(object):
+     
+    def __init__(self,dv_name,value,DVmapping,lower,upper):
+        
+        '''Create a geometic desing variable with specified mapping
+
+        Input:
+        
+        dv_name: Design variable name. Should be unique. Can be used
+        to set pyOpt variables directly
+
+        DVmapping: One or more mappings which relate to this design
+        variable
+
+        lower: Lower bound for the variable. Again for setting in
+        pyOpt
+
+        upper: Upper bound for the variable. '''
+
+
+        self.name = dv_name
+        self.value = value
+        self.lower = lower
+        self.upper = upper
+        self.DVmapping = DVmapping
+
+        return
+
+    def applyValue(self,surf,s):
+        '''Set the actual variable. Surf is the spline surface.'''
+        
+        self.DVmapping.apply(surf,s,self.value)
+        
+
+        return
+
+
+class DVmapping(object):
+
+    def __init__(self,sec_start,sec_end,apply_to,formula):
+
+        '''Create a generic mapping to apply to a set of b-spline control
+        points.
+
+        Input:
+        
+        sec_start: j index (spanwise) where mapping function starts
+        sec_end : j index (spanwise) where mapping function
+        ends. Python-based negative indexing is allowed. eg. -1 is the last element
+
+        apply_to: literal reference to select what planform variable
+        the mapping applies to. Valid litteral string are:
+                'x'  -> X-coordinate of the reference axis
+                'y'  -> Y-coordinate of the reference axis
+                'z'  -> Z-coordinate of the reference axis
+                'twist' -> rotation about the z-axis
+                'x-rot' -> rotation about the x-axis
+                'y-rot' -> rotation about the x-axis
+
+        formula: is a string which contains a python expression for
+        the mapping. The value of the mapping is assigned as
+        'val'. Distance along the surface is specified as 's'. 
+
+        For example, for a linear shearing sweep, the formula would be 's*val'
+        '''
+        self.sec_start = sec_start
+        self.sec_end   = sec_end
+        self.apply_to  = apply_to
+        self.formula   = formula
+
+        return
+
+    def apply(self,surf,s,val):
+        '''apply mapping to surface'''
+
+        if self.apply_to == 'x':
+#             print 'ceofs'
+#             print surf.coef[0,0,self.sec_start:self.sec_end,0]
+#             print surf.coef[0,0,:,0]
+#             print 'formula'
+#             print eval(self.formula)
+
+            surf.coef[:,:,:,0] += eval(self.formula)
+
+#            surf.coef[:,:,self.sec_start:self.sec_end,0]+= eval(self.formula)
+#             print 'formula:',self.formula
+#             print 's:',s
+#             print 'val:',val
+            print eval(self.formula).shape
+            print 'done x'
+
 #==============================================================================
 # Class Test
 #==============================================================================
--- test.py
+++ test.py
@@ -28,7 +28,7 @@ sys.path.append(os.path.abspath('../../pyACDT/pyACDT/Optimization/pyOpt/pySNOPT'
 #pyGeo
 import pyGeo
 
-#Lets start setting things we know we will need
+# #Lets start setting things we know we will need
 naf = 10
 bl_length = 21.15
 
@@ -42,6 +42,7 @@ chord = array([.6440,1.0950,1.6800,\
 #chord = ones(naf)
 sloc = array([0.0000,0.1141,\
         0.2184,0.3226,0.4268,0.5310,0.6352,0.8437,0.9479,1.0000])
+#sloc = linspace(0,1,naf)
 le_loc = array([0.5000,0.3300,\
           0.2500,0.2500,0.2500,0.2500,0.2500,0.2500,0.2500,0.2500])
 #le_loc = 0.25*ones(naf)
@@ -52,25 +53,44 @@ airfoil_list = ['af1-6.inp','af-07.inp','af8-9.inp','af8-9.inp','af-10.inp',\
 #                  'af15-16.inp','af15-16.inp','af15-16.inp','af15-16.inp',
 #                  'af15-16.inp','af15-16.inp']
 
-#airfoil_list = ['af-11.inp','af-11.inp','af-11.inp','af-11.inp',
+# airfoil_list = ['af-11.inp','af-11.inp','af-11.inp','af-11.inp',
 #                 'af-11.inp','af-11.inp','af-11.inp','af-11.inp',
-#                 'af-11.inp','af-11.inp','af-11.inp']
+#                 'af-11.inp','af-11.inp']
 
+# #Transport aircraft wing example
+# naf = 10
+# bl_length = 5.0
+# tw_aero = linspace(1.5,-1,naf)
+# chord = linspace(1.5,0.3,naf)
+# le_loc = 0.25*ones(naf)
+# sloc = linspace(0,1,naf)
+# airfoil_list = ['af15-16.inp','af15-16.inp','af15-16.inp','af15-16.inp',
+#                 'af15-16.inp','af15-16.inp','af15-16.inp','af15-16.inp',
+#                 'af15-16.inp','af15-16.inp']
 ref_axis = zeros((naf,3))
 ref_axis[:,2] = sloc*bl_length
 ref_axis[:,1] = 0
 ref_axis[:,0] = 0
 
-#rot_x = -arctan(3*sloc/bl_length)*180/pi
-#rot_y = arctan(2*sloc/bl_length)*180/pi
 rot_x = zeros(naf)
 rot_y = zeros(naf)
 
-geobj = pyGeo.pyGeo(bl_length,ref_axis,le_loc,chord,tw_aero,rot_x,rot_y,airfoil_list,N=13)
+geobj = pyGeo.pyGeo(ref_axis,le_loc,chord,tw_aero,rot_x,rot_y,airfoil_list,N=15)
 
 geobj.createSurface()
-geobj.joinSurfaces()
+geobj.surf.writeTecplot('output.dat')
 
-geobj.writeSurfaceTecplot(50,150,'output.dat')
+sweep_map = pyGeo.DVmapping(sec_start = 0, sec_end = -1, apply_to='x',formula='0+s*val')
+geobj.addVar('sweep',0,sweep_map,lower=0,upper=40)
+# dihedral_map = pyGeo.DVmapping(sec_start = 0, sec_end = -1, apply_to='y',formula='0+s*val')
+# geobj.addVar('dihedral',0,sweep_map,lower=0,upper=10)
+
+geobj.DVlist['sweep'].value = 10
+
+geobj.updateDV()
 
 
+geobj.surf.writeTecplot('output_def.dat')
+
+
+print 'Done!'
