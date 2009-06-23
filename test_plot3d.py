27146df8c7644f103544eb6c60e96fc550f59c77
Gaetan Kenway <gaetank@gmail.com>
2009-06-23 23:55:32 +0000

    more work on splines

---
+++ test_plot3d.py
@@ -0,0 +1,40 @@
+# =============================================================================
+# Standard Python modules
+# =============================================================================
+import os, sys, string, pdb, copy, time
+
+# =============================================================================
+# External Python modules
+# =============================================================================
+from numpy import linspace, cos, pi, hstack, zeros, ones, sqrt, imag, interp, \
+    array, real, reshape, meshgrid, dot, cross
+
+# =============================================================================
+# Extension modules
+# =============================================================================
+
+#pyOPT
+sys.path.append(os.path.abspath('../../../../pyACDT/pyACDT/Optimization/pyOpt/'))
+
+# pySpline
+sys.path.append('../pySpline/python')
+
+#pyOPT
+sys.path.append(os.path.abspath('../../pyACDT/pyACDT/Optimization/pyOpt/'))
+
+#pySNOPT
+sys.path.append(os.path.abspath('../../pyACDT/pyACDT/Optimization/pyOpt/pySNOPT'))
+
+#pyGeo
+import pyGeo
+
+aircraft = pyGeo.pyGeo('full_aircraft.xyz')
+
+f = open('plot3d_example.dat','w')
+f.write ('VARIABLES = "X", "Y","Z"\n')
+for ipatch in xrange(aircraft.nPatch):
+    print 'outputing patch %d'%(ipatch)
+    aircraft.surfs[ipatch].writeTecplot(handle=f)
+
+f.close()
+
