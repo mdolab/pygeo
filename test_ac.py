a8885fc9f5323f0bfda9035ee5907b85f3bcc3b6
Gaetan Kenway <gaetank@gmail.com>
2009-07-04 13:49:43 +0000

    Added a bunch of stuff

--- test_ac.py
+++ test_ac.py
@@ -28,41 +28,66 @@ sys.path.append(os.path.abspath('../../pyACDT/pyACDT/Optimization/pyOpt/pySNOPT'
 #pyGeo
 import pyGeo
 
+# # Wind Turbine Blade
+
+# naf = 10
+# airfoil_list = ['af1-6.inp','af-07.inp','af8-9.inp','af8-9.inp','af-10.inp',\
+#                     'af-11.inp','af-12.inp', 'af-14.inp',\
+#                     'af15-16.inp','af15-16.inp']
+
+# chord = array([.6440,1.0950,1.6800,\
+#          1.5390,1.2540,0.9900,0.7900,0.4550,0.4540,0.4530])
+
+# sloc = array([0.0000,0.1141,\
+#         0.2184,0.3226,0.4268,0.5310,0.6352,0.8437,0.9479,1.0000])*10
+
+# tw_aero = array([0.0,20.3900,16.0200,11.6500,\
+#                 6.9600,1.9800,-1.8800,-3.4100,-3.4500,-3.4700])
+
+# ref_axis = pyGeo.ref_axis(2.5*ones(naf),zeros(naf),sloc,zeros(naf),zeros(naf),tw_aero)
+
+# le_loc = array([0.5000,0.3300,\
+#           0.2500,0.2500,0.2500,0.2500,0.2500,0.2500,0.2500,0.2500])
+
+# offset = zeros([naf,2])
+# offset[:,0] = le_loc
+# blade  = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis,fit_type='lms',Nctlu=13,Nctlv=naf)
+
+# #blade.writeTecplot('wing.dat')
+# #blade.writeIGES('wing.igs')
+
+# blade.stitchPatches(1e-3,1e-2) #node_tol,edge_tol
+
+
+
+# Fuselage
+
 naf = 10
-airfoil_list = ['af1-6.inp','af-07.inp','af8-9.inp','af8-9.inp','af-10.inp',\
-                    'af-11.inp','af-12.inp', 'af-14.inp',\
-                    'af15-16.inp','af15-16.inp']
+airfoil_list = ['af1-6.inp','af1-6.inp','af1-6.inp','af1-6.inp','af1-6.inp',
+                'af1-6.inp','af1-6.inp','af1-6.inp','af1-6.inp','af1-6.inp']
 
-chord = array([.6440,1.0950,1.6800,\
-         1.5390,1.2540,0.9900,0.7900,0.4550,0.4540,0.4530])
-
-sloc = array([0.0000,0.1141,\
-        0.2184,0.3226,0.4268,0.5310,0.6352,0.8437,0.9479,1.0000])*10
-
-tw_aero = array([0.0,20.3900,16.0200,11.6500,\
-                6.9600,1.9800,-1.8800,-3.4100,-3.4500,-3.4700])
-
-ref_axis = pyGeo.ref_axis(zeros(naf),zeros(naf),sloc,zeros(naf),zeros(naf),tw_aero)
-
-le_loc = array([0.5000,0.3300,\
-          0.2500,0.2500,0.2500,0.2500,0.2500,0.2500,0.2500,0.2500])
+chord = array([.1,.4,.45,.46,.45,.3,.15,.1,.08,.07])
+sloc  = linspace(0,5,naf)
+plot(sloc,chord)
+show()
+ref_axis = pyGeo.ref_axis(sloc,zeros(naf),zeros(naf),0*ones(naf),90*ones(naf),90*ones(naf))
 
 offset = zeros([naf,2])
-offset[:,0] = le_loc
-blade  = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis,fit_type='lms',Nctlu=13,Nctlv=naf)
+offset[:,0] = 0.5
 
-#blade.writeTecplot('wing.dat')
-#blade.writeIGES('wing.igs')
+fuse  = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis,fit_type='lms',Nctlu=13,Nctlv=naf)
+fuse.writeTecplot('fuse.dat')
+fuse.writeIGES('fuse.igs')
 
-blade.stitchPatches(1e-3,1e-2) #node_tol,edge_tol
+# Wing
 
+airfoil_list = ['af15-16.inp','af15-16.inp','af15-16.inp','af15-16.inp']
+chord = [1.25,1,.8,.65]
+tw_aero = [-4,0,4,4.5]
+ref_axis = pyGeo.ref_axis([1.25,1.25,1.25,1.25],[0,0.1,0.2,0.4],[0,2,4,6],[00,00,00,0],[0,0,0,0],tw_aero)
+offset = zeros((4,2))
+offset[:,0] = .25 #1/4 chord
+wing = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis,fit_type='lms',Nctlu = 13,Nctlv= naf)
+wing.writeTecplot('wing.dat')
+wing.writeIGES('wing.igs')
 
-
-# airfoil_list = ['af15-16.inp','af15-16.inp','af15-16.inp']
-# chord = [1,1,1]
-# tw_aero = [0,0,0]
-# ref_axis = pyGeo.ref_axis([0,0,0],[0,0,0],[0,2,4],[00,00,00],[0,0,0],[0,0,0])
-# offset = zeros((3,2))
-# vstab = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis)
-# #vstab.writeTecplot('straight_test.dat')
-# vstab.writeIGES('straight_test.igs')
--- test_iges.py
+++ test_iges.py
@@ -28,6 +28,20 @@ sys.path.append(os.path.abspath('../../pyACDT/pyACDT/Optimization/pyOpt/pySNOPT'
 #pyGeo
 import pyGeo
 
-aircraft  = pyGeo.pyGeo('iges',file_name='sailplane_split.igs')
-aircraft.writeTecplot('sailplane_split.dat')
-aircraft.stitchPatches()
+aircraft  = pyGeo.pyGeo('iges',file_name='DPW4_wb_no_tail_v03.igs')
+aircraft.writeTecplot('DPW.dat')
+#aircraft.stitchPatches(1e-1,1e-1)
+
+#print 'knot vectors:'
+
+# for i in xrange(4):
+#     print 'surf %d'%(i)
+#     print 'tu:',aircraft.surfs[i].tu
+# #    print 'ceofs:',aircraft.surfs[i].coef
+
+# print 
+# print
+# for i in xrange(4):
+#     print 'surf %d'%(i)
+#     print 'tv:',aircraft.surfs[i].tv
+
