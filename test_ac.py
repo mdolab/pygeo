60dae24f1287d14f0d3b39ba4b125747ee1c294a
Gaetan Kenway <gaetank@gmail.com>
2009-07-16 22:14:52 +0000

    update to pyGeo

--- test_ac.py
+++ test_ac.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # =============================================================================
 # Standard Python modules
 # =============================================================================
@@ -13,71 +14,15 @@ from numpy import linspace, cos, pi, hstack, zeros, ones, sqrt, imag, interp, \
 # Extension modules
 # =============================================================================
 from matplotlib.pylab import *
-#pyOPT
-sys.path.append(os.path.abspath('../../../../pyACDT/pyACDT/Optimization/pyOpt/'))
 
 # pySpline
 sys.path.append('../pySpline/python')
 
-#pyOPT
-sys.path.append(os.path.abspath('../../pyACDT/pyACDT/Optimization/pyOpt/'))
-
-#pySNOPT
-sys.path.append(os.path.abspath('../../pyACDT/pyACDT/Optimization/pyOpt/pySNOPT'))
-
 #pyGeo
 import pyGeo
 
-# # Wind Turbine Blade
+# Wing Information
 
-# naf = 10
-# airfoil_list = ['af1-6.inp','af-07.inp','af8-9.inp','af8-9.inp','af-10.inp',\
-#                     'af-11.inp','af-12.inp', 'af-14.inp',\
-#                     'af15-16.inp','af15-16.inp']
-
-# chord = array([.6440,1.0950,1.6800,\
-#          1.5390,1.2540,0.9900,0.7900,0.4550,0.4540,0.4530])
-
-# sloc = array([0.0000,0.1141,\
-#         0.2184,0.3226,0.4268,0.5310,0.6352,0.8437,0.9479,1.0000])*10
-
-# tw_aero = array([0.0,20.3900,16.0200,11.6500,\
-#                 6.9600,1.9800,-1.8800,-3.4100,-3.4500,-3.4700])
-
-# ref_axis = pyGeo.ref_axis(2.5*ones(naf),zeros(naf),sloc,zeros(naf),zeros(naf),tw_aero)
-
-# le_loc = array([0.5000,0.3300,\
-#           0.2500,0.2500,0.2500,0.2500,0.2500,0.2500,0.2500,0.2500])
-
-# offset = zeros([naf,2])
-# offset[:,0] = le_loc
-# blade  = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis,fit_type='lms',Nctlu=13,Nctlv=naf)
-
-# #blade.writeTecplot('wing.dat')
-# #blade.writeIGES('wing.igs')
-
-# blade.stitchPatches(1e-3,1e-2) #node_tol,edge_tol
-
-# # Fuselage
-
-# naf = 10
-# airfoil_list = ['af1-6.inp','af1-6.inp','af1-6.inp','af1-6.inp','af1-6.inp',
-#                 'af1-6.inp','af1-6.inp','af1-6.inp','af1-6.inp','af1-6.inp']
-
-# chord = array([.1,.4,.45,.46,.45,.3,.15,.1,.08,.07])
-# sloc  = linspace(0,5,naf)
-# plot(sloc,chord)
-# show()
-# ref_axis = pyGeo.ref_axis(sloc,zeros(naf),zeros(naf),0*ones(naf),90*ones(naf),90*ones(naf))
-
-# offset = zeros([naf,2])
-# offset[:,0] = 0.5
-
-# fuse  = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis,fit_type='lms',Nctlu=13,Nctlv=naf)
-# fuse.writeTecplot('fuse.dat')
-# fuse.writeIGES('fuse.igs')
-
-# Wing
 naf=8
 airfoil_list = ['af15-16.inp','af15-16.inp','af15-16.inp','af15-16.inp','af15-16.inp','af15-16.inp','af15-16.inp','pinch.inp']
 chord = [1.25,1,.8,.65,.65,0.65,.65,.65]
@@ -86,65 +31,79 @@ y = [0,0.1,0.2,0.4,.405,.55,.6,1.2]
 z = [0,2,4,6,6.05,6.2,6.2,6.2]
 rot_x = [0,0,0,0,0,-90,-90,-90]
 rot_y = [0,0,0,0,0,0,0,0]
-tw_aero = [-4,0,4,4.5,4.5,0,0,0]
+tw_aero = [-4,0,4,4.5,4.5,0,0,0] # ie rot_z
 ref_axis1 = pyGeo.ref_axis(x,y,z,rot_x,rot_y,tw_aero)
 offset = zeros((naf,2))
 offset[:,0] = .25 #1/4 chord
 
 # Make the break-point vector
-breaks = [3,6]
+breaks = [3,6] #zero based
 Nctlv = [4,4,4] # Length breaks + 1
-wing = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis1,breaks=breaks,fit_type='lms',Nctlu = 13,Nctlv=Nctlv)
 
-# #Corner
-# naf=4
-# airfoil_list = ['af15-16.inp','af15-16.inp','af15-16.inp','af15-16.inp']
-# chord = [.65,.65,.65,.65]
+# Procedure for Using pyGEO
 
-# ref_axis2 = pyGeo.ref_axis([1.25,1.25,1.25,1.25],[0.4,.405,.55,.6],[6,6.05,6.20,6.20],[0,0,-90,-90],[0,0,0,0],[4.5,4.5,0,0])
-# offset = zeros((4,2))
-# offset[:,0] = .25 #1/4 chord
-# corner = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis2=ref_axis,fit_type='lms',Nctlu = 13,Nctlv= naf)
+# Step 1: Run the folloiwng Commands: (Uncomment between -------)
+# ---------------------------------------------------------------------
+# wing = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis1,breaks=breaks,fit_type='lms',Nctlu = 13,Nctlv=Nctlv)
+# wing.calcEdgeConnectivity(1e-2,1e-2)
+# wing.writeEdgeConnectivity('wing.con') wing.writeTecplot('wing.dat')
+# print 'Done Step 1' sys.exit(0)
+# ----------------------------------------------------------------------
+# Now: -> Load wing.dat to check connectivity information and modifiy
+# wing.con file to correct any connectivity info and set
+# continuity. Re-run step 1 until all connectivity information and
+# continutity information is correct.
 
-# #Winglet
-# naf=2
-# airfoil_list = ['af15-16.inp','pinch.inp']
-# chord = [.65,.65]
+# Step 2: -> Run the following Commands (Uncomment between --------)
+# After step 1 we can load connectivity information from file,
+# propagate the knot vectors, stitch the edges, and then fit the
+# entire surfaces with continuity constraints.  This output is then
+# saved as an igs file which is the archive format storage format we
+# are using for bspline surfaces
 
-# ref_axis3 = pyGeo.ref_axis([1.25,1.25],[.6,1.2],[6.2,6.2],[-90,-90],[0,0],[0,0])
-# offset = zeros((2,2))
-# offset[:,0] = .25 #1/4 chord
+# ----------------------------------------------------------------------
+# wing = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis=ref_axis1,breaks=breaks,fit_type='lms',Nctlu = 13,Nctlv=Nctlv)
+# wing.readEdgeConnectivity('wing.con')
+# wing.propagateKnotVectors()
+# wing.stitchEdges()
+# #wing.fitSurfaces()
+# wing.writeTecplot('wing.dat')
+# wing.writeIGES('wing.igs')
+# print 'Done Step 2'
+# sys.exit(0)
+# ----------------------------------------------------------------------
 
-# winglet = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,scale=chord,offset=offset,ref_axis3=ref_axis,fit_type='lms',Nctlu = 13,Nctlv= naf)
+# Step 3: -> After step 2 we now have two files we need, the stored
+# iges file as well as the connectivity file. The IGES file which has
+# been generated can be used to generate a 3D CFD mesh in ICEM.  Now
+# to load a geometry for an optimization run we simply load the IGES
+# file as well as the connectivity file and we are good to go.
 
-# # Now add everything to the wing:
-#wing.addGeoObject(corner)
-#del corner
-#wing.addGeoObject(winglet)
-#del winglet
+# ----------------------------------------------------------------------
 
-wing.calcEdgeConnectivity(1e-2,1e-2)
+wing = pyGeo.pyGeo('iges',file_name='wing.igs')
+wing.readEdgeConnectivity('wing.con')
+wing.stitchEdges() # Just to be sure
+print 'Done Step 3'
 
-wing.writeEdgeConnectivity('test.con')
-#wing.loadEdgeConnectivity('test2.con')
-wing.propagateKnotVectors()
-wing.stitchEdges()
-#wing.fitSurfaces()
+# ----------------------------------------------------------------------
 
+# Step 4: Now the rest of the code is up to the user. The user only
+# needs to run the commands in part 3 to fully define the geometry of
+# interest
 
 print 'Attaching Ref Axis...'
 wing.setRefAxis([0,1,2,3,4,5],ref_axis1)
 
 wing.writeTecplot('wing.dat',ref_axis1)
 
-
 print 'modifiying ref axis:'
 
 ref_axis1.x[:,2] *= 1.2
 ref_axis1.x[:,0] += 3*ref_axis1.xs.s
-ref_axis1.x[:,1] += 0.1*ref_axis1.xs.s**2
+ref_axis1.x[:,1] += 0.4*ref_axis1.xs.s**2
 
 wing.update(ref_axis1)
-
+wing.stitchEdges() # Just to be sure
 wing.writeTecplot('wing2.dat',ref_axis1)
 
