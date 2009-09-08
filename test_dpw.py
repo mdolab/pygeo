cf4d07fbf3185a5893ecb6f3a66ce650924e0ff8
kenway <kenway@localhost>
2009-09-08 20:22:36 +0000

    pyGeo UPdate

--- test_dpw.py
+++ test_dpw.py
@@ -26,7 +26,7 @@ import pyGeo2 as pyGeo
 # outputted by aerosurf. It then creates a b-spline surfaces for each
 # # surface patch.
 # timeA = time.time()
-# aircraft = pyGeo.pyGeo('plot3d',file_name='fuse2.xyz')
+# aircraft = pyGeo.pyGeo('plot3d',file_name='dpw.xyz')
 # aircraft.calcEdgeConnectivity()
 # aircraft.writeEdgeConnectivity('dpw.con')
 # #aircraft.readEdgeConnectivity('aircraft.con')
@@ -69,14 +69,14 @@ aircraft.addRefAxis([2,3,8,9],X[0:2,:],rot[0:2,:],nrefsecs=6)
 aircraft.addRefAxis([4,5,10,11,16,17],X[1:3,:],rot[1:3,:],nrefsecs=6)
 aircraft.addRefAxisCon(0,1,'end') # Innter Wing and Outer Wing ('Attach ra1 to ra0 with type 'end')
 
-
+coef0 = copy.deepcopy(aircraft.surfs[3].coef)
 def span_extension(val,ref_axis):
     '''Single design variable for span extension'''
                        
 #    ref_axis[0].x[:,1] = ref_axis[0].x0[:,1]*1.5
 
     ref_axis[0].rot[:,1] = val
-
+    ref_axis[1].rot[:,1] = val
     return ref_axis
 
 
@@ -86,8 +86,145 @@ aircraft.addGeoDVGlobal('span',1,0.5,2.0,span_extension)
 idg = aircraft.DV_namesGlobal #NOTE: This is constant (idg -> id global
 print 'idg',idg
 
-aircraft.DV_listGlobal[idg['span']].value =8
+aircraft.DV_listGlobal[idg['span']].value =45
 
 aircraft.update()
-aircraft.checkCoef()
+#aircraft.checkCoef()
 aircraft.writeTecplot('dpw.dat',edges=True,links=True)
+
+
+
+# Start of mesh warping testing
+
+
+
+coef = copy.deepcopy(aircraft.surfs[3].coef)
+Nu = coef.shape[0]
+Nv = coef.shape[1]
+
+
+file_name = 'warp_test.dat'
+f = open(file_name ,'w')
+f.write ('VARIABLES = "X", "Y","Z"\n')
+f.write('Zone T=%s I=%d J=%d\n'%('coef',Nu,Nv))
+f.write('DATAPACKING=POINT\n')
+for j in xrange(Nv):
+    for i in xrange(Nu):
+        f.write('%f %f %f \n'%(coef0[i,j,0],coef0[i,j,1],coef0[i,j,2]))
+    # end for
+# end for
+
+# Pluck out a section of coefficients
+# Now parameterize it:
+
+S = zeros([Nu,Nv,2])
+
+# The low ends should be set
+# Find DelI and DelJ
+def delI(i,j,vals):
+    return sqrt( ( vals[i,j,0]-vals[i-1,j,0]) ** 2 + \
+                  (vals[i,j,1]-vals[i-1,j,1]) ** 2 + \
+                  (vals[i,j,2]-vals[i-1,j,2]) ** 2)
+
+def delJ(i,j,vals):
+    return sqrt( ( vals[i,j,0]-vals[i,j-1,0]) ** 2 + \
+                  (vals[i,j,1]-vals[i,j-1,1]) ** 2 + \
+                  (vals[i,j,2]-vals[i,j-1,2]) ** 2)
+
+
+for i in xrange(1,Nu):
+    for j in xrange(1,Nv):
+        S[i,j,0] = S[i-1,j  ,0] + delI(i,j,coef)
+        S[i,j,1] = S[i  ,j-1,1] + delJ(i,j,coef)
+
+for i in xrange(1,Nu):
+    S[i,0,0] = S[i-1,0,0] + delI(i,0,coef)
+for j in xrange(1,Nv):
+    S[0,j,1] = S[0,j-1,1] + delJ(0,j,coef)
+
+
+# Do a no-check normalization
+for i in xrange(Nu):
+    for j in xrange(Nv):
+        S[i,j,0] /= S[-1,j,0]
+        S[i,j,1] /= S[i,-1,1]
+
+dface = zeros((Nu,Nv,3))
+
+# Set up corner perturbations:
+
+dface[0,0] = coef[0,0]-coef0[0,0]
+dface[0,-1] = coef[0,-1]-coef0[0,-1]
+dface[-1,0] = coef[-1,0]-coef0[-1,0]
+dface[-1,-1] = coef[-1,-1]-coef0[-1,-1]
+
+# Edge 0
+for i in xrange(1,Nu):
+    j = 0
+    WTK2 = S[i,j,0]
+    WTK1 = 1.0-WTK2
+    
+    dface[i,j] = WTK1 * dface[0,j] + WTK2 * dface[-1,j]
+
+# Edge 1
+for i in xrange(1,Nu):
+    j = -1
+    WTK2 = S[i,j,0]
+    WTK1 = 1.0-WTK2
+    dface[i,j] = WTK1 * dface[0,j] + WTK2 * dface[-1,j]
+
+# Edge 1
+for j in xrange(1,Nv):
+    i=0
+    WTK2 = S[i,j,1]
+    WTK1 = 1.0-WTK2
+    dface[i,j] = WTK1 * dface[i,0] + WTK2 * dface[i,-1]
+
+# Edge 1
+for j in xrange(1,Nv):
+    i=-1
+    WTK2 = S[i,j,1]
+    WTK1 = 1.0-WTK2
+    dface[i,j] = WTK1 * dface[i,0] + WTK2 * dface[i,-1]
+
+eps = 1.0e-14
+
+for i in xrange(1,Nu-1):
+    for j in xrange(1,Nv-1):
+        WTI2 = S[i,j,0]
+        WTI1 = 1.0-WTI2
+        WTJ2 = S[i,j,1]
+        WTJ1 = 1.0-WTJ2
+        deli = WTI1 * dface[0,j,0] + WTI2 * dface[-1,j,0]
+        delj = WTJ1 * dface[i,0,0] + WTJ2 * dface[i,-1,0]
+       
+        dface[i,j,0] = (abs(deli)*deli + abs(delj)*delj)/  \
+            max( ( abs (deli) + abs(delj),eps))
+            
+
+        deli = WTI1 * dface[0,j,1] + WTI2 * dface[-1,j,1]
+        delj = WTJ1 * dface[i,0,1] + WTJ2 * dface[i,-1,1]
+        
+        dface[i,j,1] = (abs(deli)*deli + abs(delj)*delj)/ \
+            max( ( abs (deli) + abs(delj),eps))
+
+        deli = WTI1 * dface[0,j,2] + WTI2 * dface[-1,j,2]
+        delj = WTJ1 * dface[i,0,2] + WTJ2 * dface[i,-1,2]
+        
+        dface[i,j,2] = (abs(deli)*deli + abs(delj)*delj)/  \
+            max( ( abs (deli) + abs(delj),eps))
+    # end for
+# end for
+
+# That was for the corners. Now do it for the edges
+
+
+coef += dface
+
+f.write('Zone T=%s I=%d J=%d\n'%('coef_warp',Nu,Nv))
+f.write('DATAPACKING=POINT\n')
+for j in xrange(Nv):
+    for i in xrange(Nu):
+        f.write('%f %f %f \n'%(coef[i,j,0],coef[i,j,1],coef[i,j,2]))
+    # end for
+# end for
