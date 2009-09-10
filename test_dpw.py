a6dccfd888551141950baa29a439136e5b8c83f9
Gaetan Kenway <gaetank@gmail.com>
2009-09-10 21:54:16 +0000

    pyGeo update

--- test_dpw.py
+++ test_dpw.py
@@ -7,7 +7,7 @@ import os, sys, string, pdb, copy, time
 # External Python modules
 # =============================================================================
 from numpy import linspace, cos, pi, hstack, zeros, ones, sqrt, imag, interp, \
-    array, real, reshape, meshgrid, dot, cross
+    array, real, reshape, meshgrid, dot, cross,shape,alltrue
 
 # =============================================================================
 # Extension modules
@@ -22,7 +22,7 @@ sys.path.append('../../pyHF/pycfd-csm/python/')
 
 #pyGeo
 import pyGeo2 as pyGeo
-
+from geo_utils import *
 # This script reads a surfaced-based plot3d file as typically
 # outputted by aerosurf. It then creates a b-spline surfaces for each
 # surface patch.
@@ -66,12 +66,13 @@ aircraft.addRefAxis([2,3,8,9],X[0:2,:],rot[0:2,:],nrefsecs=6)
 aircraft.addRefAxis([4,5,10,11,16,17],X[1:3,:],rot[1:3,:],nrefsecs=6)
 aircraft.addRefAxisCon(0,1,'end') # Innter Wing and Outer Wing ('Attach ra1 to ra0 with type 'end')
 
-
 def span_extension(val,ref_axis):
     '''Single design variable for span extension'''
-  #   ref_axis[0].rot[:,1] = val
-#     ref_axis[1].rot[:,1] = val
-    ref_axis[0].scale[:] = 1.0
+    ref_axis[0].rot[:,1] = val
+    ref_axis[1].rot[:,1] = val
+    ref_axis[0].scale[:] = .9
+    #ref_axis[0].x[:,2] += 30
+   
     return ref_axis
 
 
@@ -81,191 +82,124 @@ aircraft.addGeoDVGlobal('span',1,0.5,2.0,span_extension)
 idg = aircraft.DV_namesGlobal #NOTE: This is constant (idg -> id global
 print 'idg',idg
 
-aircraft.DV_listGlobal[idg['span']].value =-10
-coef0 = copy.deepcopy(aircraft.surfs[12].coef) # Pluck out coef before update
+aircraft.DV_listGlobal[idg['span']].value =0
+# Create a coef0 list for all surfaces:
+init_coef = []
+coef_temp = []
+for isurf in xrange(aircraft.nSurf):
+    init_coef.append(copy.deepcopy(aircraft.surfs[isurf].coef)) # Pluck out coef before update
+    coef_temp.append(zeros(shape(aircraft.surfs[isurf].coef)))
+
 aircraft.update()
 
+
 # Start of mesh warping testing
-
-coef = copy.deepcopy(aircraft.surfs[12].coef)
-Nu = coef.shape[0]
-Nv = coef.shape[1]
-
-file_name = 'warp_test.dat'
-f = open(file_name ,'w')
-f.write ('VARIABLES = "X", "Y","Z"\n')
-f.write('Zone T=%s I=%d J=%d\n'%('coef',Nu,Nv))
-f.write('DATAPACKING=POINT\n')
-for j in xrange(Nv):
-    for i in xrange(Nu):
-        f.write('%f %f %f \n'%(coef0[i,j,0],coef0[i,j,1],coef0[i,j,2]))
-    # end for
-# end for
-
 # Pluck out a section of coefficients
 # Now parameterize it:
 
-S = zeros([Nu,Nv,2])
+surfaces_to_warp = [0,1,6,7,12,13]
+surfaces_to_warp = [0,12]#,13]#0,1,6,7,12,13]
+timeA = time.time()
 
-# The low ends should be set
-# Find DelI and DelJ
-def delI(i,j,vals):
-    return sqrt( ( vals[i,j,0]-vals[i-1,j,0]) ** 2 + \
-                  (vals[i,j,1]-vals[i-1,j,1]) ** 2 + \
-                  (vals[i,j,2]-vals[i-1,j,2]) ** 2)
+for isurf in surfaces_to_warp:
+    # Check ONLY the edges
+    coef0 = copy.deepcopy(init_coef[isurf])
+    coef  = aircraft.surfs[isurf].coef
 
-def delJ(i,j,vals):
-    return sqrt( ( vals[i,j,0]-vals[i,j-1,0]) ** 2 + \
-                  (vals[i,j,1]-vals[i,j-1,1]) ** 2 + \
-                  (vals[i,j,2]-vals[i,j-1,2]) ** 2)
+    print 'isurf:',isurf
+    Nu = aircraft.surfs[isurf].Nctlu
+    Nv = aircraft.surfs[isurf].Nctlv
 
-for i in xrange(1,Nu):
-    for j in xrange(1,Nv):
-        S[i,j,0] = S[i-1,j  ,0] + delI(i,j,coef)
-        S[i,j,1] = S[i  ,j-1,1] + delJ(i,j,coef)
-
-for i in xrange(1,Nu):
-    S[i,0,0] = S[i-1,0,0] + delI(i,0,coef)
-for j in xrange(1,Nv):
-    S[0,j,1] = S[0,j-1,1] + delJ(0,j,coef)
-
-# Do a no-check normalization
-for i in xrange(Nu):
-    for j in xrange(Nv):
-        S[i,j,0] /= S[-1,j,0]
-        S[i,j,1] /= S[i,-1,1]
-
-dface = zeros((Nu,Nv,2))
-
-# Set up corner perturbations:
-
-dface[0,0] = 0
-dface[0,-1] = 0
-#Hard code this up:
-
-temp = pySpline.linear_spline('interpolate',X=coef0[:,0],k=2)
-u = temp.projectPoint(coef[-1,0])
-
-temp = pySpline.linear_spline('interpolate',X=coef0[-1,:],k=2)
-v = temp.projectPoint(coef[-1,0])
-
-print 'u,v:',u,v
-dface[-1,0] = [u[0]-1,v[0]-0]
-dface[-1,-1] = 0
-
-# Edge 0
-for i in xrange(1,Nu):
-    j = 0
-    WTK2 = S[i,j,0]
-    WTK1 = 1.0-WTK2
-    dface[i,j] = WTK1 * dface[0,j] + WTK2 * dface[-1,j]
-
-# Edge 1
-for i in xrange(1,Nu):
-    j = -1
-    WTK2 = S[i,j,0]
-    WTK1 = 1.0-WTK2
-    dface[i,j] = WTK1 * dface[0,j] + WTK2 * dface[-1,j]
-
-# Edge 1
-for j in xrange(1,Nv):
-    i=0
-    WTK2 = S[i,j,1]
-    WTK1 = 1.0-WTK2
-    dface[i,j] = WTK1 * dface[i,0] + WTK2 * dface[i,-1]
-
-# Edge 1
-for j in xrange(1,Nv):
-    i=-1
-    WTK2 = S[i,j,1]
-    WTK1 = 1.0-WTK2
-    dface[i,j] = WTK1 * dface[i,0] + WTK2 * dface[i,-1]
-
-eps = 1.0e-14
-
-print 'S:',S[:,:,0]
-
-for i in xrange(1,Nu-1):
-    for j in xrange(1,Nv-1):
-        WTI2 = S[i,j,0]
-        WTI1 = 1.0-WTI2
-        WTJ2 = S[i,j,1]
-        WTJ1 = 1.0-WTJ2
-        deli = WTI1 * dface[0,j,0] + WTI2 * dface[-1,j,0]
-        delj = WTJ1 * dface[i,0,0] + WTJ2 * dface[i,-1,0]
+    S = parameterizeFace(Nu,Nv,coef0)
+    dface = zeros((Nu,Nv,2))
        
-        dface[i,j,0] = (abs(deli)*deli + abs(delj)*delj)/  \
-            max( ( abs (deli) + abs(delj),eps))
+    # Spline the edges:
+    # Create the matrix of spline objects for all the lines:
+    u_lines = []
+    v_lines = []
+    for j in xrange(Nv):
+        # Make the u-lines:
+        u_lines.append(pySpline.linear_spline('interpolate',X=coef0[:,j],k=2))
+    for i in xrange(Nu):
+        v_lines.append(pySpline.linear_spline('interpolate',X=coef0[i,:],k=2))
+       
+
+    # Now we can get the all the parameteric dface peturbation on the edges
+    timeB = time.time()
+    # Edge 0 and Edge 1
+    counter = 0
+    for i in xrange(Nu):
+        if not alltrue(coef[i,0]==coef0[i,0]):
+           #  print 'here0'
+            u,D,converged,update = u_lines[0].projectPoint(coef[i,0])
+            v,D,converged,update = v_lines[i].projectPoint(coef[i,0])
+            dface[i,0] = [u-S[i,0,0],v-S[i,0,1]]
+            counter += 1
+
+        if not alltrue(coef[i,-1]==coef0[i,-1]):
+           # print 'here1'
+            u,D,converged,update = u_lines[-1].projectPoint(coef[i,-1])
+            v,D,converged,update = v_lines[i].projectPoint(coef[i,-1])
+            dface[i,-1] = [u-S[i,-1,0],v-S[i,-1,1]]
+            counter += 1
+    # end for
+
+    # Edge 2 and Edge 3
+    for j in xrange(Nv):
+        if not alltrue(coef[0,j]==coef0[0,j]):
+            #print 'here2'
             
-
-        deli = WTI1 * dface[0,j,1] + WTI2 * dface[-1,j,1]
-        delj = WTJ1 * dface[i,0,1] + WTJ2 * dface[i,-1,1]
-        
-        dface[i,j,1] = (abs(deli)*deli + abs(delj)*delj)/ \
-            max( ( abs (deli) + abs(delj),eps))
+            u,D,converged,update = u_lines[j].projectPoint(coef[0,j])
+            v,D,converged,update = v_lines[0].projectPoint(coef[0,j])
+           
+            dface[0,j] = [u-S[0,j,0],v-S[0,j,1]]
+            counter += 1
+        if not alltrue(coef[-1,j]==coef0[-1,j]):
+            #print 'here3'
+            u,D,converged,update = u_lines[j].projectPoint(coef[-1,j])
+            v,D,converged,update = v_lines[-1].projectPoint(coef[-1,j])
+            dface[-1,j] = [u-S[-1,j,0],v-S[-1,j,1]]
+            counter += 1
     # end for
-# end for
+   # print 'counter:',counter
 
-print 'dface check:',dface[:,:,1]
 
-# Now we know the 'parametric movements' we can make splines for each 'line' and move 'em accordingly
-
-# Add the displacements in quadrature
-
-dtemp = zeros([Nv,Nv,2,3])
-
-for i in xrange(Nu):
-    for j in xrange(Nv):
-        temp = pySpline.linear_spline('interpolate',X=coef[:,j],k=2)
-        try_val = S[i,j,0]+dface[i,j,0]
-        if try_val > 1:
-            try_val = 1
-        if try_val < 0:
-            try_val = 0
-        
-        dtemp[i,j,0] = temp.getValue(try_val)-coef[i,j]
-
-for j in xrange(Nv):
+    # Now interpolate the rest of dface with the parametric algebraic
+    # warping algorithim
+    timeC = time.time()
+    dface = warp_face(Nu,Nv,S,dface)
+    timeD = time.time()
+    # Now we know the 'parametric movements' we can make splines for each 'line' and move 'em accordingly
     for i in xrange(Nu):
-        temp = pySpline.linear_spline('interpolate',X=coef[i,:],k=2)
-        try_val = S[i,j,1]+dface[i,j,1]
-        if try_val > 1:
-            try_val = 1
-        if try_val < 0:
-            try_val = 0
-
-
-        dtemp[i,j,1] = temp.getValue(try_val)-coef[i,j]
-
-# Now merge the dtmeps and set back to coef
-
-for i in xrange(Nu):
-    for j in xrange(Nv):
-        coef[i,j] = coef[i,j] + dtemp[i,j,0] + dtemp[i,j,1]
-
-
-
-
-
-print 'dtemp:',dtemp[-1,0,1]+dtemp[-1,0,0]
-
-
-f.write('Zone T=%s I=%d J=%d\n'%('coef_warp',Nu,Nv))
-f.write('DATAPACKING=POINT\n')
-for j in xrange(Nv):
-    for i in xrange(Nu):
-        f.write('%f %f %f \n'%(coef[i,j,0],coef[i,j,1],coef[i,j,2]))
+        for j in xrange(Nv):
+            # Physical Change caused by du and dv
+            dx_u = u_lines[j].getValue(S[i,j,0]+dface[i,j,0]) - coef0[i,j]
+            dx_v = v_lines[i].getValue(S[i,j,1]+dface[i,j,1]) - coef0[i,j]
+            coef[i,j] = coef0[i,j] + dx_u + dx_v
+        # end for
     # end for
-# end for
-print 'done'
+            
+    coef_temp[isurf] = coef
+    timeE = time.time()
+# end for (isurf loop)
 
 
-# # Force set those coef in the global list:
+print 'Init Time:',timeB-timeA
+print 'Project Time:',timeC-timeB
+print 'Warp Time:',timeD-timeC
+print 'Set time:',timeE-timeD
 
-l_list = aircraft.l_index[12]
-for i in xrange(Nu):
-    for j in xrange(Nv):
-        aircraft.coef[l_list[i,j]] = coef[i,j]
-
+ # # Force set those coef in the global list:
+for isurf in surfaces_to_warp:
+    l_list = aircraft.l_index[isurf]
+    for i in xrange(Nu):
+        for j in xrange(Nv):
+            aircraft.coef[l_list[i,j]] = coef_temp[isurf][i,j]
+        # end for
+    # end for
 aircraft._updateSurfaceCoef()
-aircraft.writeTecplot('dpw.dat',edges=True,directions=True,labels=True)
+
+aircraft.writeTecplot('dpw.dat',edges=True,directions=True,
+                      labels=True,links=True)
+print 'coef0[-1,0]',coef0[-1,0]
+print 'coef[-1,0]',coef[-1,0]
