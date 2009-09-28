335cab653bd6b861ee93be3ba822789e8ed52f77
Gaetan Kenway <gaetank@gmail.com>
2009-09-28 21:22:59 +0000

    pyGeo update

--- examples/test_nmg1.py
+++ examples/test_nmg1.py
@@ -33,21 +33,31 @@ from geo_utils import *
 # ==============================================================================
 
 #Load in the plot3D file to test:
-#eo = pyGeo.pyGeo('plot3d',file_name='../input/nmg1.xyz.fmt')
+#geo = pyGeo.pyGeo('plot3d',file_name='../input/nmg1.xyz.fmt')
 #geo = pyGeo.pyGeo('plot3d',file_name='../input/loopy.xyz')
-geo = pyGeo.pyGeo('plot3d',file_name='../input/full_aircraft.xyz',no_print=True)
+geo = pyGeo.pyGeo('plot3d',file_name='../input/full_aircraft.xyz')
+# del geo.surfs[1]
+# del geo.surfs[2]
+# del geo.surfs[2]
+# del geo.surfs[2]
+# del geo.surfs[2]
+# del geo.surfs[2]
+# del geo.surfs[2]
+# del geo.surfs[2]
+# del geo.surfs[2]
+# del geo.surfs[-2]
+# geo.nSurf = 4
 
-timeA = time.time()
-geo.calcEdgeConnectivity()
-timeB = time.time()
-print 'Edge Calc Time:',timeB-timeA
+#geo.calcEdgeConnectivity()
+#geo.printEdgeConnectivity()
+#geo.writeEdgeConnectivity('nmg1.con')
 
-geo.printEdgeConnectivity()
-geo.writeEdgeConnectivity('nmg1.con')
-geo.readEdgeConnectivity('nmg1.con')
-geo.printEdgeConnectivity()
+geo.readEdgeConnectivity('long-ez.con')
+geo.propagateKnotVectors()
 
-for i in xrange(len(geo.l_index)):
-    print geo.l_index[i]
-geo.writeTecplot('../output/npg1.dat',orig=True)
+geo.fitSurfaces()
+geo.update()
+# for i in xrange(len(geo.l_index)):
+#     print geo.l_index[i]
+geo.writeTecplot('../output/npg1.dat',orig=True,nodes=False)
 print ' Done!'
