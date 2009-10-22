fa979bb7dfbd32c3535d1ce7025afcaeed2e1806
kenway <kenway@localhost>
2009-10-22 21:23:54 +0000

    pyLayout /pyGeo upadte

--- examples/bwb/bwb.py
+++ examples/bwb/bwb.py
@@ -36,8 +36,8 @@ sys.path.append('../../')
 import pyGeo_NM as pyGeo
 
 #pyLayout
-#sys.path.append('../../../pyLayout/')
-#import pyLayout_NM as pyLayout
+sys.path.append('../../../pyLayout/')
+import pyLayout_NM as pyLayout
 
 #Design Variable Functions
 sys.path.append('../')
@@ -142,20 +142,25 @@ chord/=SCALE
 # Step 1: Run the folloiwng Commands: (Uncomment between -------)
 # ---------------------------------------------------------------------
 #Note: u direction is chordwise, v direction is span-wise
-bwb = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,
-                  file_type='xfoil',scale=chord,offset=offset, 
-                  breaks=breaks,cont=cont,nsections=nsections,
-                  end_type='rounded',end_scale =1,
-                  Xsec=X,rot=rot,fit_type='lms',Nctlu=Nctlu,Nfoil=45)
+# bwb = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,
+#                   file_type='xfoil',scale=chord,offset=offset, 
+#                   breaks=breaks,cont=cont,nsections=nsections,
+#                   end_type='rounded',end_scale =1,
+#                   Xsec=X,rot=rot,fit_type='lms',Nctlu=Nctlu,Nfoil=45)
 
-bwb.setSymmetry('xy')
-#bwb.calcEdgeConnectivity(1e-6,1e-6)
-#bwb.writeEdgeConnectivity('bwb.con')
+# bwb.setSymmetry('xy')
+# #bwb.calcEdgeConnectivity(1e-6,1e-6)
+# #bwb.writeEdgeConnectivity('bwb.con')
+# bwb.readEdgeConnectivity('bwb.con')
+# bwb.propagateKnotVectors()
+# bwb.fitSurfaces3(nIter=40,constr_tol=1e-8,opt_tol=1e-6)
+# bwb.writeTecplot('../../output/bwb.dat',orig=True,nodes=True)
+# bwb.writeIGES('../../input/bwb.igs')
+
+# Read the IGES file
+bwb = pyGeo.pyGeo('iges',file_name='../../input/bwb.igs')
 bwb.readEdgeConnectivity('bwb.con')
-bwb.propagateKnotVectors()
-bwb.fitSurfaces3(nIter=40,constr_tol=1e-8,opt_tol=1e-6)
-bwb.writeTecplot('../../output/bwb.dat',orig=True,nodes=True)
-bwb.writeIGES('../../input/bwb.igs')
+
 
 print '---------------------------'
 print 'Attaching Reference Axis...'
@@ -170,17 +175,16 @@ bwb.addGeoDVGlobal('span',1,0.5,2.0,span_extension)
 bwb.calcCtlDeriv()
 print 'Done Ref Axis Adding!'
 
-sys.exit(0)
-
 print '---------------------------'
 print '      pyLayout Setup' 
 print '---------------------------'
  
-MAX_SPARS = 5  # This is the same for each spanwise section
-Nsection = 1
+MAX_SPARS = 2  # This is the same for each spanwise section
+Nsection = 4
 wing_box = pyLayout.Layout(bwb,Nsection,MAX_SPARS)
 
-# ---------- Create the First Domain -------------
+# Create the full set of rib data for the main body/wing section (out
+# as far as winglet blend)
 
 # Load in the digitized data
 # Use the digitize it data for the planform:
@@ -192,12 +196,15 @@ rib_list     = array(loadtxt('bwb_rib_list.out'))
 le_spar_spline = pySpline.linear_spline(task='interpolate',k=2,X=le_spar_list[:,1],s=le_spar_list[:,0])
 te_spar_spline = pySpline.linear_spline(task='interpolate',k=2,X=te_spar_list[:,1],s=te_spar_list[:,0])
 
-MAX_RIBS = len(rib_list)
 
-le_list = zeros((MAX_RIBS,3)) # Make them the same as the Rib list...not necessary but should work
-te_list = zeros((MAX_RIBS,3)) # Make them the same as the Rib list...not necessary but should work
+# ---------- Create the First Domain -- First 8 ribts
 
-span_coord = rib_list[:,0]
+MAX_RIBS1 = 8
+
+le_list = zeros((MAX_RIBS1,3)) # Make them the same as the Rib list...not necessary but should work
+te_list = zeros((MAX_RIBS1,3)) # Make them the same as the Rib list...not necessary but should work
+
+span_coord = rib_list[0:MAX_RIBS1,0]
 le_list[:,2] = span_coord # Use the same spanwise cordinates (z-axis)
 le_list[:,0] = le_spar_spline.getValueV(span_coord)
 le_list[:,1] = mid_y_spline.getValueV(span_coord)
@@ -206,40 +213,185 @@ te_list[:,2] = span_coord
 te_list[:,0] = te_spar_spline.getValueV(span_coord)
 te_list[:,1] = mid_y_spline.getValueV(span_coord)
 
-domain = pyLayout.domain(le_list,te_list)
+le_list/=SCALE
+te_list/=SCALE
+domain1 = pyLayout.domain(le_list.copy(),te_list.copy())
+
 
 # # ---------- OPTIONAL SPECIFIC RIB DISTIRBUTION -----------
 
 #Comput the physical rib position
-rib_pos = zeros((MAX_RIBS,3))
-rib_basis = rib_list[:,0] # This is the basis
+rib_pos = zeros((MAX_RIBS1,3))
+rib_basis = rib_list[0:MAX_RIBS1,0] # This is the basis
 rib_pos[:,2] = rib_basis
 rib_pos[:,1] = mid_y_spline.getValueV(rib_basis)
-rib_pos[:,0] = rib_list[:,1]
-
-rib_dir = zeros((MAX_RIBS,3))
+rib_pos[:,0] = rib_list[0:MAX_RIBS1,1]
+rib_pos/=SCALE
+rib_dir = zeros((MAX_RIBS1,3))
 rib_dir[:] = [1,0,0]
 
 # # -----------------------------------------------------------
 
-rib_blank = ones((MAX_RIBS,MAX_SPARS-1))
-spar_blank = ones((MAX_SPARS,MAX_RIBS-1))
+rib_blank = ones((MAX_RIBS1,MAX_SPARS-1))
+spar_blank = ones((MAX_SPARS,MAX_RIBS1-1))
 
 # Spacing Parameters for Elements
-span_space = 1*ones(MAX_RIBS-1)
-rib_space  = 1*ones(MAX_SPARS+1) # Note the +1
-v_space    = 1
+span_space = 1*ones(MAX_RIBS1-1)
+span_space[0] = 2
+span_space[1] = 2
+span_space[2] = 4
+
+rib_space  = 3*ones(MAX_SPARS+1) # Note the +1
+v_space    = 3
 
 surfs = [[0],[1]] #Upper surfs for LE to TE then Lower Surfs from LE to TE
-spar_con = [0,0,0,0,0]
+spar_con = [0,0]
 
-timeA = time.time()
-
-def1 = pyLayout.struct_def(MAX_RIBS,MAX_SPARS,domain,surfs,spar_con,
+def1 = pyLayout.struct_def(MAX_RIBS1,MAX_SPARS,domain1,surfs,spar_con,
                            rib_blank=rib_blank,rib_pos=rib_pos,rib_dir=rib_dir,
                            spar_blank=spar_blank,
                            span_space = span_space,rib_space=rib_space,v_space=v_space)
                            
 wing_box.addSection(def1)
+
+# # ---------- Create the Second Domain -- Last First 19 ribs
+
+MAX_RIBS2 = 19
+
+le_list = zeros((MAX_RIBS2,3)) # Make them the same as the Rib list...not necessary but should work
+te_list = zeros((MAX_RIBS2,3)) # Make them the same as the Rib list...not necessary but should work
+
+span_coord = rib_list[MAX_RIBS1-1:MAX_RIBS1+MAX_RIBS2,0]
+
+le_list[:,2] = span_coord # Use the same spanwise cordinates (z-axis)
+le_list[:,0] = le_spar_spline.getValueV(span_coord)
+le_list[:,1] = mid_y_spline.getValueV(span_coord)
+
+te_list[:,2] = span_coord
+te_list[:,0] = te_spar_spline.getValueV(span_coord)
+te_list[:,1] = mid_y_spline.getValueV(span_coord)
+
+le_list/=SCALE
+te_list/=SCALE
+
+domain2 = pyLayout.domain(le_list,te_list)
+
+# # ---------- OPTIONAL SPECIFIC RIB DISTIRBUTION -----------
+
+#Comput the physical rib position
+rib_pos = zeros((MAX_RIBS2,3))
+rib_basis = rib_list[MAX_RIBS1-1:MAX_RIBS1+MAX_RIBS2,0] # This is the basis
+rib_pos[:,2] = rib_basis
+rib_pos[:,1] = mid_y_spline.getValueV(rib_basis)
+rib_pos[:,0] = rib_list[MAX_RIBS1-1:MAX_RIBS1+MAX_RIBS2,1]
+rib_pos/=SCALE
+rib_dir = zeros((MAX_RIBS2,3))
+rib_dir[:] = [1,0,0]
+
+# # -----------------------------------------------------------
+
+rib_blank = ones((MAX_RIBS2,MAX_SPARS-1))
+spar_blank = ones((MAX_SPARS,MAX_RIBS2-1))
+
+# Spacing Parameters for Elements
+span_space = 1*ones(MAX_RIBS2-1)
+rib_space  = 3*ones(MAX_SPARS+1) # Note the +1
+v_space    = 3
+
+surfs = [[2],[3]] #Upper surfs for LE to TE then Lower Surfs from LE to TE
+spar_con = [2,2]
+
+def2 = pyLayout.struct_def(MAX_RIBS2,MAX_SPARS,domain2,surfs,spar_con,
+                           rib_blank=rib_blank,rib_pos=rib_pos,rib_dir=rib_dir,
+                           spar_blank=spar_blank,
+                           span_space = span_space,rib_space=rib_space,v_space=v_space)
+                           
+wing_box.addSection(def2)
+
+
+# # ---------- Create the Third Domain Wing Blend
+
+MAX_RIBS3 = 3
+
+le_list3 = zeros((MAX_RIBS3,3)) # Make them the same as the Rib list...not necessary but should work
+te_list3 = zeros((MAX_RIBS3,3)) # Make them the same as the Rib list...not necessary but should work
+
+le_list3[0] = le_list[-1]
+te_list3[0] = te_list[-1]
+le_list3[1] = bwb.surfs[4].getValue(0,0.5)+[.025,0,0]
+te_list3[1] = bwb.surfs[4].getValue(1,0.5)-[.025,0,0]
+le_list3[2] = bwb.surfs[4].getValue(0,1) + [.025,0,0]
+te_list3[2] = bwb.surfs[4].getValue(1,1) - [.025,0,0]
+
+domain3 = pyLayout.domain(le_list3,te_list3)
+
+# # ---------- OPTIONAL SPECIFIC RIB DISTIRBUTION -----------
+
+#Comput the physical rib position
+rib_pos = zeros((MAX_RIBS3,3))
+rib_pos = 0.5*(le_list3+te_list3)
+rib_dir = zeros((MAX_RIBS3,3))
+rib_dir[:] = [1,0,0]
+print 'rib_pos:',rib_pos
+# # -----------------------------------------------------------
+
+rib_blank = ones((MAX_RIBS3,MAX_SPARS-1))
+spar_blank = ones((MAX_SPARS,MAX_RIBS3-1))
+
+# Spacing Parameters for Elements
+span_space = 1*ones(MAX_RIBS3-1)
+rib_space  = 3*ones(MAX_SPARS+1) # Note the +1
+v_space    = 3
+
+surfs = [[4],[5]] #Upper surfs for LE to TE then Lower Surfs from LE to TE
+spar_con = [4,4]
+
+def3 = pyLayout.struct_def(MAX_RIBS3,MAX_SPARS,domain3,surfs,spar_con,
+                           rib_blank=rib_blank,rib_pos=rib_pos,rib_dir=rib_dir,
+                           spar_blank=spar_blank,
+                           span_space = span_space,rib_space=rib_space,v_space=v_space)
+                           
+wing_box.addSection(def3)
+
+
+# # ---------- Create the Fourth Domain Winglet
+
+MAX_RIBS4 = 8
+
+le_list4 = zeros((MAX_RIBS4,3)) # Make them the same as the Rib list...not necessary but should work
+te_list4 = zeros((MAX_RIBS4,3)) # Make them the same as the Rib list...not necessary but should work
+
+le_list4[:] = bwb.surfs[6].getValueV(zeros(MAX_RIBS4),linspace(0,1,MAX_RIBS4)) + [.0085,0,0]
+te_list4[:] = bwb.surfs[6].getValueV(ones(MAX_RIBS4),linspace(0,1,MAX_RIBS4)) - [.005,0,0]
+domain4 = pyLayout.domain(le_list4,te_list4)
+
+# # ---------- OPTIONAL SPECIFIC RIB DISTIRBUTION -----------
+
+#Comput the physical rib position
+rib_pos = zeros((MAX_RIBS4,3))
+rib_pos = 0.5*(le_list4+te_list4)
+rib_dir = zeros((MAX_RIBS4,3))
+rib_dir[:] = [1,0,0]
+# # -----------------------------------------------------------
+
+rib_blank = ones((MAX_RIBS4,MAX_SPARS-1))
+spar_blank = ones((MAX_SPARS,MAX_RIBS4-1))
+
+# Spacing Parameters for Elements
+span_space = 1*ones(MAX_RIBS4-1)
+rib_space  = 3*ones(MAX_SPARS+1) # Note the +1
+v_space    = 3
+
+surfs = [[6],[7]] #Upper surfs for LE to TE then Lower Surfs from LE to TE
+spar_con = [6,6]
+
+def4 = pyLayout.struct_def(MAX_RIBS4,MAX_SPARS,domain4,surfs,spar_con,
+                           rib_blank=rib_blank,rib_pos=rib_pos,rib_dir=rib_dir,
+                           spar_blank=spar_blank,
+                           span_space = span_space,rib_space=rib_space,v_space=v_space)
+                           
+wing_box.addSection(def4)
+
+
 wing_box.writeTecplot('../../output/bwb_layout.dat')
 wing_box.finalize()
--- examples/test_pyLayout.py
+++ examples/test_pyLayout.py
@@ -50,24 +50,23 @@ from dv_funcs import *
 
 # Wing Information - Create a Geometry Object from cross sections
 
-naf=2
-airfoil_list = ['../input/naca0012.dat','../input/naca0012.dat']
-chord = [1,1]
-x = [0,0]
-y = [0,0]
-z = [0,3.94]
-rot_x = [0,0]
-rot_y = [0,0]
-tw_aero = [0,0]
+naf=3
+airfoil_list = ['../input/naca0012.dat','../input/naca0012.dat','../input/naca0012.dat']
+chord = [1,1,.5]
+x = [0,0,.25]
+y = [0,0,0]
+z = [0,2,4]
+rot_x = [0,0,0]
+rot_y = [0,0,0]
+tw_aero = [0,0,0]
 
 offset = zeros((naf,2))
 offset[:,0] = .25 # Offset sections by 0.25 in x
-section_spacing = [linspace(0,1,10)]
-nsections = [10]
+nsections = [6,6]
 # Make the break-point vector
 Nctlu = 13
 end_type = 'rounded'
-                               
+breaks = [1]                               
 # Put spatial and rotations into two arrays (always the same)-------
 X = zeros((naf,3))
 rot = zeros((naf,3))
@@ -80,27 +79,27 @@ rot[:,1] = rot_y
 rot[:,2] = tw_aero
 # ------------------------------------------------------------------
 # #Note: u direction is chordwise, v direction is span-wise
-wing = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,\
-                   file_type='xfoil',scale=chord,offset=offset, \
+wing = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,
+                   file_type='xfoil',scale=chord,offset=offset, 
                    Xsec=X,rot=rot,end_type=end_type,nsections=nsections,
-                   section_spacing=section_spacing,
+                   breaks=breaks,
                    fit_type='lms', Nctlu=Nctlu,Nfoil=45)
 
-#wing.calcEdgeConnectivity(1e-6,1e-6)
-#wing.writeEdgeConnectivity('test_layout.con')
+wing.calcEdgeConnectivity(1e-6,1e-6)
+wing.writeEdgeConnectivity('test_layout.con')
+#sys.exit(0)
 wing.readEdgeConnectivity('test_layout.con')
 wing.propagateKnotVectors()
 #wing.fitSurfaces3()
-wing.writeTecplot('../output/wing.dat',labels=True,ref_axis=True,directions=True,nodes=True)
+wing.writeTecplot('../output/wing.dat')
 wing.writeIGES('../input/wing.igs')
 # ------------------------------------------------------------------
-
 print '---------------------------'
 print 'Attaching Reference Axis...'
 print '---------------------------'
 
-wing.addRefAxis([0,1,2,3],X[0:2,:],rot[0:2,:],nrefsecs=nsections[0],\
-                    spacing=section_spacing[0])
+wing.addRefAxis([0,1,2,3,4,5],X[0:2,:],rot[0:2,:],nrefsecs=nsections[0])
+                   
 print 'Done Ref Axis Adding!'
 
 print ' ** Adding Global Design Variables **'
@@ -111,17 +110,17 @@ print '---------------------------'
 print '      pyLayout Setup' 
 print '---------------------------'
  
-MAX_SPARS = 5  # This is the same for each spanwise section
-Nsection = 1
+MAX_SPARS = 3  # This is the same for each spanwise section
+Nsection = 2
 wing_box = pyLayout.Layout(wing,Nsection,MAX_SPARS)
 
 # ---------- Create the First Domain -------------
 
-MAX_RIBS = 8
-le_list = array([[-.10,0,0],[-.10,0,3.94]])
-te_list = array([[.60,0,0],[.6,0,3.94]])
+MAX_RIBS = 6
+le_list = array([[-.10,0,0],[-.10,0,2]])
+te_list = array([[.60,0,0],[.6,0,2]])
 
-domain = pyLayout.domain(le_list,te_list)
+domain1 = pyLayout.domain(le_list,te_list)
 
 # # ---------- OPTIONAL SPECIFIC RIB DISTIRBUTION -----------
 rib_pos = zeros((MAX_RIBS,3))
@@ -139,15 +138,51 @@ rib_space  = 1*ones(MAX_SPARS+1) # Note the +1
 v_space    = 1
 
 surfs = [[0],[1]] #Upper surfs for LE to TE then Lower Surfs from LE to TE
-spar_con = [0,0,0,0,0]
+spar_con = [0,0,0]
 
 timeA = time.time()
-def1 = pyLayout.struct_def(MAX_RIBS,MAX_SPARS,domain,surfs,spar_con,
+def1 = pyLayout.struct_def(MAX_RIBS,MAX_SPARS,domain1,surfs,spar_con,
                            rib_blank=rib_blank,rib_pos=rib_pos,rib_dir=rib_dir,
                            spar_blank=spar_blank,
                            span_space = span_space,rib_space=rib_space,v_space=v_space)
                            
 wing_box.addSection(def1)
+
+# ---------- Create the Second Domain -------------
+
+MAX_RIBS = 5
+le_list = array([[-.10,0,2],[.2,0,4]])
+te_list = array([[.60,0,2],[.55,0,4]])
+
+domain2 = pyLayout.domain(le_list,te_list)
+
+# # ---------- OPTIONAL SPECIFIC RIB DISTIRBUTION -----------
+rib_pos = zeros((MAX_RIBS,3))
+spline = pySpline.linear_spline(task='interpolate',k=2,X=X[1:3])
+rib_pos = spline.getValueV(linspace(0,1,MAX_RIBS))
+
+rib_dir = zeros((MAX_RIBS,3))
+rib_dir[:] = [1,0,0] # Note a rib-dir MUST be specified 
+# # -----------------------------------------------------------
+
+rib_blank = ones((MAX_RIBS,MAX_SPARS-1))
+spar_blank = ones((MAX_SPARS,MAX_RIBS-1))
+span_space = 1*ones(MAX_RIBS-1)
+rib_space  = 1*ones(MAX_SPARS+1) # Note the +1
+v_space    = 1
+
+surfs = [[2],[3]] #Upper surfs for LE to TE then Lower Surfs from LE to TE
+spar_con = [2,2,2]
+
+timeA = time.time()
+def2 = pyLayout.struct_def(MAX_RIBS,MAX_SPARS,domain2,surfs,spar_con,
+                           rib_blank=rib_blank,rib_pos=rib_pos,rib_dir=rib_dir,
+                           spar_blank=spar_blank,
+                           span_space = span_space,rib_space=rib_space,v_space=v_space)
+                           
+wing_box.addSection(def2)
+
+
 wing_box.writeTecplot('../output/layout.dat')
 wing_box.finalize()
 
