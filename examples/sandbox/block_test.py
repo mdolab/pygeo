4eb3fb19c4f2c9bc254886a9902cca759cb0e5ac
Gaetan Kenway <gaetank@gmail.com>
2010-03-12 23:17:22 +0000

    pyGeo example update

--- examples/sandbox/block_test.py
+++ examples/sandbox/block_test.py
@@ -16,42 +16,39 @@ from numpy import linspace, cos, pi, hstack, zeros, ones, sqrt, imag, interp, \
 from mdo_import_helper import *
 exec(import_modules('pyBlock','pySpline','geo_utils'))
 
-# DV_Volume Examples
-grid = pyBlock.pyBlock('plot3d',file_name='dv_blocking.fmt',file_type='ascii')
-grid.doConnectivity('grid.con')
-grid.writeBvol('dv_volume.bvol',binary=True)
-grid = pyBlock.pyBlock('bvol',file_name='dv_volume.bvol',file_type='binary')
-grid.writeTecplot('blocks.dat',vols=True,orig=False,coef=True)
-sys.exit(0)
 
-# SGS Wing Case
-# grid = pyBlock.pyBlock('plot3d',file_name='sgs.xyz',file_type='binary')
-# grid.doConnectivity('grid.con')
-# grid.writeBvol('sgs.bvol',binary=True)
-# print len(grid.topo.g_index)
+# print 'DV Volume Example'
+# grid = pyBlock.pyBlock('plot3d',file_name='dv_blocking.fmt',file_type='ascii')
+# grid.doConnectivity('dv.con')
+# grid.writeBvol('dv_volume.bvol',binary=True)
+# grid.writePlot3d('dv_volume_new.xyz')
 
-# grid = pyBlock.pyBlock('bvol',file_name='sgs.bvol',file_type='binary')
-# grid.writeTecplot('blocks.dat',vols=True,orig=False,coef=True)
+# grid = pyBlock.pyBlock('bvol',file_name='dv_volume.bvol',file_type='binary')
+# grid.doConnectivity('dv.con')
+# grid.writeTecplot('dv.dat',vols=True,orig=False,coef=True)
 
-# DPW4 Case
-#grid = pyBlock.pyBlock('plot3d',file_name='test.xyz',file_type='binary')
-#grid.doConnectivity('grid.con')
-#grid.writeBvol('dpw.bvol',binary=True)
+#print ' '
+print 'SGS Wing Example'
+grid = pyBlock.pyBlock('plot3d',file_name='sgs.xyz',file_type='binary')
+grid.doConnectivity('sgs.con')
+grid.writePlot3d('sgs_new.xyz',binary=False)
+grid.writeTecplot('sgs.dat',vols=False,orig=True,coef=False)
+grid.writeBvol('sgs.bvol',binary=True)
 
-grid = pyBlock.pyBlock('bvol',file_name='dpw.bvol',file_type='binary')
-grid.writeTecplot('blocks.dat',vols=True,orig=False,coef=True,tecio=True)
+#grid = pyBlock.pyBlock('bvol',file_name='sgs.bvol',file_type='binary')
+#grid.doConnectivity('sgs.con')
+#grid.writeTecplot('sgs.dat',vols=True,orig=False,coef=True)
 
 
+print ' '
+print 'DPW4 Example'
+grid = pyBlock.pyBlock('plot3d',file_name='test.xyz',file_type='binary')
+grid.doConnectivity('dpw.con')
+grid.writeTecplot('blocks.dat',vols=True,orig=True,coef=False)
+grid.writePlot3d('dpw_new.xyz',binary=False)
+grid.writeBvol('dpw.bvol',binary=True)
 
-# print len(grid.topo.g_index)
+#grid = pyBlock.pyBlock('bvol',file_name='dpw.bvol',file_type='binary')
+#grid.doConnectivity('dpw.con')
 
-# N = grid.topo.l_index[3][0,-1,-1]
-
-# grid.coef[N] += [0,0,20]
-# grid._updateVolumeCoef()
-# grid.writeTecplot('blocks_mod.dat',vols=True,orig=False,coef=False)
-
-# #grid = pyGeo.pyBlock('plot3d',file_name='test.xyz',file_type='binary')
-
-# #grid.writeFEAPCorners('corners.in')
-
+print len(grid.topo.g_index)
