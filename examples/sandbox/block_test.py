22d58324ec747c4ffa77c9f170d3b66a072b8daf
Gaetan Kenway <gaetank@gmail.com>
2010-03-16 20:09:58 +0000

    pyGeo update

--- examples/sandbox/block_test.py
+++ examples/sandbox/block_test.py
@@ -2,7 +2,9 @@
 # Standard Python modules
 # =============================================================================
 import os, sys, string, pdb, copy, time
- 
+#import psyco
+#psyco.full()
+
 # =============================================================================
 # External Python modules
 # =============================================================================
@@ -25,30 +27,33 @@ exec(import_modules('pyBlock','pySpline','geo_utils'))
 
 # grid = pyBlock.pyBlock('bvol',file_name='dv_volume.bvol',file_type='binary')
 # grid.doConnectivity('dv.con')
-# grid.writeTecplot('dv.dat',vols=True,orig=False,coef=True)
+# grid.writeTecplot('dv.dat',vols=True,orig=False,coef=True,tecio=True)
+
 
 #print ' '
-print 'SGS Wing Example'
-grid = pyBlock.pyBlock('plot3d',file_name='sgs.xyz',file_type='ascii')
-grid.doConnectivity('sgs.con')
-grid.writePlot3d('sgs_new.xyz',binary=False)
-grid.writeTecplot('sgs.dat',vols=False,orig=True,coef=False)
-grid.writeBvol('sgs.bvol',binary=True)
-sys.exit(0)
-#grid = pyBlock.pyBlock('bvol',file_name='sgs.bvol',file_type='binary')
-#grid.doConnectivity('sgs.con')
-#grid.writeTecplot('sgs.dat',vols=True,orig=False,coef=True)
+# print 'SGS Wing Example'
+# grid = pyBlock.pyBlock('plot3d',file_name='sgs.xyz',file_type='ascii')
+# grid.doConnectivity('sgs.con')
+# grid.fitGlobal()
+# sys.exit(0)
+# grid.writeTecplot('sgs1.dat',vols=True,orig=False,coef=False,tecio=False)
+# grid.writeBvol('sgs.bvol',binary=False)
+
+# grid = pyBlock.pyBlock('bvol',file_name='sgs.bvol',file_type='ascii')
+# grid.doConnectivity('sgs.con')
+# grid.writeTecplot('sgs2.dat',vols=True,orig=False,coef=False,tecio=False)
+
 
 
 print ' '
 print 'DPW4 Example'
 grid = pyBlock.pyBlock('plot3d',file_name='test.xyz',file_type='binary')
 grid.doConnectivity('dpw.con')
-grid.writeTecplot('blocks.dat',vols=True,orig=True,coef=False)
+grid.fitGlobal()
+sys.exit(0)
+grid.writeTecplot('blocks.dat',vols=True,orig=True,coef=False,tecio=True)
 grid.writePlot3d('dpw_new.xyz',binary=False)
 grid.writeBvol('dpw.bvol',binary=True)
 
 #grid = pyBlock.pyBlock('bvol',file_name='dpw.bvol',file_type='binary')
 #grid.doConnectivity('dpw.con')
-
-print len(grid.topo.g_index)
