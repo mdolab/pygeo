07ba5b491cc74aecbf59af361548685ac866b111
Gaetan Kenway <gaetank@gmail.com>
2010-04-01 18:36:05 +0000

    minor pygeo update

--- examples/sandbox/warp_test.py
+++ examples/sandbox/warp_test.py
@@ -27,9 +27,6 @@ grid.fitGlobal()
 grid.writeBvol('warp_test.bvol',binary=True)
 grid.writeTecplot('warp_test.dat',tecio=True,orig=True)
 
-# grid = pyBlock.pyBlock('bvol',file_name='warp_test.bvol',file_type='binary')
-# grid.doConnectivity('warp_test.con')
-
 g_index,gptr,l_index,lptr,l_sizes = grid.topo.flatten_indices()
 mpiPrint('Number of Unique Nodes in Mesh: %d'%(len(grid.topo.g_index)))
 
@@ -38,7 +35,8 @@ print 'Generating Surface Geometry'
 surface = pyGeo.pyGeo('plot3d',file_name='warp_test_surf.xyz',file_type='ascii',order='f')
 surface.doConnectivity('warp_test_surf.con')
 surface.fitGlobal()
-#surface.writeTecplot('warp_test_surf.dat',tecio=True,coef=True,orig=False,surf_labels=True,directions=True)
+surface.writeTecplot('warp_test_surf.dat',tecio=True,coef=True,orig=False,surf_labels=True,directions=True)
+
 #surface.addFFD('auto',nx=2,ny=3,nz=10)
 #surface.FFD.writeTecplot('ffd.dat')
 
@@ -77,33 +75,40 @@ cfd_surface_points = solver.interface.Mesh.GetGlobalSurfaceCoordinates()
 surface.attachSurface(cfd_surface_points[:,:,0])
 
 #--------------  Do an ad-hoc modification --------------
-# Pull out LE
-for j in xrange(surface.surfs[4].Nctlv):
-    surface.coef[surface.topo.l_index[4][1,j]] += [-.15,0,0]
-    surface.coef[surface.topo.l_index[4][2,j]] += [-.15,0,0]
+#surface.coef[surface.topo.l_index[2][:,:]] += [.400,.200,0]
+#surface.coef[surface.topo.l_index[1][-1,:]] += [-.05,.002525,0]
+#surface.coef[surface.topo.l_index[0][-1,:]] += [.05,.000062525,0]
 
-# Shrink TE
+# # Pull out LE
+for j in xrange(surface.surfs[4].Nctlv):
+    surface.coef[surface.topo.l_index[4][1,j]] += [-.05,0,0]
+    surface.coef[surface.topo.l_index[4][2,j]] += [-.05,0,0]
+
+# # Shrink TE
 for i in xrange(surface.surfs[2].Nctlu):
     for j in xrange(surface.surfs[2].Nctlv):
-        surface.coef[surface.topo.l_index[2][i,j]][1] *= .02 
+        surface.coef[surface.topo.l_index[2][i,j]][1] *= .175 
 
-# Pull Up Upper surface
+# # Pull Up Upper surface
 for j in xrange(surface.surfs[0].Nctlv):
-    surface.coef[surface.topo.l_index[0][1,j]] += [0,.03,0]
+    surface.coef[surface.topo.l_index[0][1,j]] += [0,.08,0]
     surface.coef[surface.topo.l_index[0][2,j]] += [0,-.02,0]
 
-# Pull Down Lower surface
+# # Pull Down Lower surface
 for j in xrange(surface.surfs[3].Nctlv):
-    surface.coef[surface.topo.l_index[3][1,j]] -= [0,.05,0]
-    surface.coef[surface.topo.l_index[3][2,j]] -= [0,.04,0]
+    surface.coef[surface.topo.l_index[3][1,j]] -= [0,.09,0]
+    surface.coef[surface.topo.l_index[3][2,j]] -= [0,.03,0]
 
 surface._updateSurfaceCoef()
 surface.writeTecplot('warp_test_surf_update.dat',coef=False,tecio=True,directions=True)
+
 # ---------------------------------------------------------
 
 # Now do the solid warp
 timeA = time.time()
 solver.interface.Mesh.SetGlobalSurfaceCoordinates(surface.getSurfacePoints(0).transpose())
 solver.interface.Mesh.warpMeshSolid(g_index,gptr,l_index,lptr,l_sizes,nFree,nSurface,nBoundary)
+solver.interface.Mesh.WriteMeshFile('warp_test_new.cgns')
+
 print 'Time is:',time.time()-timeA
 
