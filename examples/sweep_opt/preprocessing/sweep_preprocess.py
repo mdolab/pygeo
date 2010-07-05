b08fbac12fa3f4787c780c0d167b26b1027d6169
kenway <kenway@localhost>
2010-07-05 19:29:05 +0000

    added the sweep example

---
+++ examples/sweep_opt/preprocessing/sweep_preprocess.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# =============================================================================
+# Standard Python modules
+# =============================================================================
+import os, sys, string, pdb, copy, time
+
+# =============================================================================
+# External Python modules
+# =============================================================================
+from numpy import linspace, cos, pi, hstack, zeros, ones, sqrt, imag, interp, \
+    array, real, reshape, meshgrid, dot, cross, vstack, arctan2, tan, loadtxt,\
+    savetxt,append
+
+# =============================================================================
+# Extension modules
+# =============================================================================
+from mdo_import_helper import *
+exec(import_modules('pyGeo','pySpline'))
+
+# ==============================================================================
+# Start of Script
+# ==============================================================================
+
+# Script to Generate a Wing Geometry
+
+naf=2
+airfoil_list = ['foil.dat',
+                'foil.dat']
+
+# Preprocess the sections to use either the design or actual coordinates
+
+Nctl = 13
+chord = array([1,1])
+
+x = [0,0]
+y = [0,0]
+z = [0,5]
+rot_x = zeros(naf)
+rot_y = zeros(naf)
+rot_z = zeros(naf)
+
+offset = zeros((naf,2))
+
+wing = pyGeo.pyGeo('lifting_surface',xsections=airfoil_list,
+                   scale=chord,offset=offset,x=x,y=y,z=z,
+                   rot_x=rot_x,rot_y=rot_y,rot_z=rot_z,Nctl=Nctl,
+                   k_span=2,con_file='arw2.con',tip='rounded')
+wing.setSymmetry('xy')
+wing.writeIGES('sweep.igs')
+wing.writeTecplot('sweep.dat')
