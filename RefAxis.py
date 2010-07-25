d85a43bb1589a6f29d2f8c1386e63fa70a5cdad4
Gaetan Kenway <gaetank@gmail.com>
2010-07-25 14:30:35 +0000

    added a uncomplete version of RefAxis.py and the sweep_opt example

---
+++ RefAxis.py
@@ -0,0 +1,235 @@
+# =============================================================================
+# RefAxis utiliy function for pyGeo and pyBlock
+# =============================================================================
+
+from numpy import pi,cos,sin,linspace,zeros,where,interp,sqrt,hstack,dot,\
+    array,max,min,insert,delete,empty,mod,tan,ones,argsort,mod,sort,\
+    arange,copy,floor,fromfile,choose,sign,resize,append,mgrid,average,cross
+from numpy.linalg import norm
+import string ,sys, copy, pdb, os,time
+
+from mdo_import_helper import *
+exec(import_modules('geo_utils','pyNetwork','pySpline'))
+
+class RefAxis(pyNetwork.pyNetwork):
+
+    def __init__(self,curves,points,rot_type=0,*args,**kwargs):
+
+        ''' Create a reference axis network from a pyNetwork object
+        
+        Reqruied:
+            network: The pyNetwork object to use
+            points:  The spatial points on which the ref axis acts
+
+        Optional:
+        
+            rot_type: integer 0-6 to determine the rotation order
+                0 -> None   -> Intrinsic rotation, rot_x is roation about axis
+                1 -> x-y-z
+                2 -> x-z-y
+                3 -> y-z-x  -> This example (right body x-streamwise y-out wing z-up)
+                4 -> y-x-z
+                5 -> z-x-y  -> Default aerosurf (Left body x-streamwise y-up z-out wing)
+                6 -> z-y-x
+        '''
+       
+        # We need to define 4 additional scalar splines on the the
+        # same basis as each spline in the network object
+        
+        self.DV_listGlobal  = [] # Global Design Variable List
+        self.DV_namesGlobal = {} # Names of Global Design Variables
+        
+        pyNetwork.pyNetwork.__init__(self,curves,*args,**kwargs)
+        self.doConnectivity()
+        self.points = points
+        self.nPt = len(points)
+
+        # New setup splines for the rotations
+        self.rot_x = []
+        self.rot_y = []
+        self.rot_z = []
+        self.scale = []
+        self.scale_x = []
+        self.scale_y = []
+        self.scale_z = []
+        for i in xrange(len(self.curves)):
+            t = self.curves[i].t
+            k = self.curves[i].k
+            N = len(self.curves[i].coef)
+            self.rot_x.append(pySpline.curve(t=t,k=k,coef=zeros((N,1))))
+            self.rot_y.append(pySpline.curve(t=t,k=k,coef=zeros((N,1))))
+            self.rot_z.append(pySpline.curve(t=t,k=k,coef=zeros((N,1))))
+
+            self.scale.append(pySpline.curve(t=t,k=k,coef=ones((N,1))))
+            self.scale_x.append(pySpline.curve(t=t,k=k,coef=ones((N,1))))
+            self.scale_y.append(pySpline.curve(t=t,k=k,coef=ones((N,1))))
+            self.scale_z.append(pySpline.curve(t=t,k=k,coef=ones((N,1))))
+        # end for
+
+        self.scale0 = copy.deepcopy(self.scale)
+        self.scale_x0 = copy.deepcopy(self.scale)
+        self.scale_y0 = copy.deepcopy(self.scale)
+        self.scale_z0 = copy.deepcopy(self.scale)
+
+        self.rot_type = rot_type
+        self.curveIDs,s = self.projectPoints(points)
+
+        self.coef0 = self.coef.copy()
+
+        self.links_s = s
+        self.links_x = []
+        self.links_n = []
+        for i in xrange(self.nPt):
+            self.links_x.append(points[i] - self.curves[self.curveIDs[i]](s[i]))
+            deriv = self.curves[self.curveIDs[i]].getDerivative(self.links_s[i])
+            deriv /= norm(deriv) # Normalize
+            self.links_n.append(cross(deriv,self.links_x[-1]))
+        # end for
+        
+        
+    def writeTecplot(self,file_name,links=True,*args,**kwargs):
+
+        '''Write the stuff out to a file'''
+
+        f = pySpline.openTecplot(file_name,3)
+
+        # Ref axis curve themselves
+        for icurve in xrange(self.nCurve):
+            self.curves[icurve]._writeTecplotCurve(f)
+
+        # Ref axis coefficients
+        for icurve in xrange(self.nCurve):
+            self.curves[icurve]._writeTecplotCoef(f)
+
+        # Write out links
+        if links:
+            self._writeTecplotLinks(f)
+        # end if
+
+        pySpline.closeTecplot(f)
+
+
+    def _writeTecplotLinks(self,handle):
+        '''Write out the surface links. '''
+
+        num_vectors = self.nPt
+        coords = zeros((2*num_vectors,3))
+        icoord = 0
+    
+        for i in xrange(self.nPt):
+            coords[icoord    ,:] = self.curves[self.curveIDs[i]](self.links_s[i])
+            coords[icoord +1 ,:] = self.points[i]
+            icoord += 2
+        # end for
+
+        icoord = 0
+        conn = zeros((num_vectors,2))
+        for ivector  in xrange(num_vectors):
+            conn[ivector,:] = icoord, icoord+1
+            icoord += 2
+        # end for
+
+        handle.write('Zone T= %s N= %d ,E= %d\n'%('links',2*num_vectors, num_vectors) )
+        handle.write('DATAPACKING=BLOCK, ZONETYPE = FELINESEG\n')
+
+        for n in xrange(3):
+            for i in  range(2*num_vectors):
+                handle.write('%f\n'%(coords[i,n]))
+            # end for
+        # end for
+
+        for i in range(num_vectors):
+            handle.write('%d %d \n'%(conn[i,0]+1,conn[i,1]+1))
+        # end for
+
+        return
+
+    
+    def addGeoDVGlobal(self,dv_name,value,lower,upper,function,useit=True):
+        '''Add a global design variable
+        Required:
+            dv_name: a unquie name for this design variable (group)
+            lower: the lower bound for this design variable
+            upper: The upper bound for this design variable
+            function: the python function for this design variable
+        Optional:
+            use_it: Boolean flag as to weather to ignore this design variable
+        Returns:
+            None
+            '''
+
+
+        self.DV_listGlobal.append(geoDVGlobal(\
+                dv_name,value,lower,upper,function,useit))
+        self.DV_namesGlobal[dv_name]=len(self.DV_listGlobal)-1
+        return
+
+
+    def update(self):
+
+        '''This is pretty straight forward, perform the operations on
+        the ref axis according to the design variables, then return
+        the list of points provided. It is up to the user to know what
+        to do with the points
+        '''
+        
+        # Step 1: Call all the design variables
+
+        for i in xrange(len(self.DV_listGlobal)):
+            self.DV_listGlobal[i](self)
+
+        self._updateCurveCoef()
+
+        # Step 2: Update the points
+        new_pts = zeros((self.nPt,3))
+        for ipt in xrange(self.nPt):
+            base_pt = self.curves[self.curveIDs[ipt]](self.links_s[ipt])
+
+            scale = self.scale[self.curveIDs[ipt]](self.links_s[ipt]) 
+            scale_x = self.scale_x[self.curveIDs[ipt]](self.links_s[ipt]) 
+            scale_y = self.scale_y[self.curveIDs[ipt]](self.links_s[ipt]) 
+            scale_z = self.scale_z[self.curveIDs[ipt]](self.links_s[ipt]) 
+
+            if self.rot_type == 0:
+                deriv = self.curves[self.curveIDs[ipt]].getDerivative(self.links_s[ipt])
+                deriv /= norm(deriv) # Normalize
+                new_vec = -cross(deriv,self.links_n[ipt])
+                new_vec = rotVbyW(new_vec,deriv,self.rot_x[self.curveIDs[ipt]](self.links_s[ipt])*pi/180)
+                new_pts[ipt] = base_pt + new_vec*scale
+            # end if
+            else:
+
+                rotX = rotxM(self.rot_x[self.curveIDs[ipt]](self.links_s[ipt]))
+                rotY = rotyM(self.rot_y[self.curveIDs[ipt]](self.links_s[ipt]))
+                rotZ = rotzM(self.rot_z[self.curveIDs[ipt]](self.links_s[ipt]))
+
+                D = self.links_x[ipt]
+                if self.rot_type == 5: # Rotate by z -x - y 
+                    D = dot(rotY,dot(rotX,dot(rotZ,D)))
+                    D[0] *= scale_x
+                    D[1] *= scale_y
+                    D[2] *= scale_z
+
+                    new_pts[ipt] = base_pt + D*scale
+                else:
+                    print 'Not Done Yet'
+                    sys.exit(0)
+                # end if
+            # end if
+         # end for
+                    
+        return new_pts
+
+    def _getRotMatrixGlobalToLocal(self,s):
+        
+        '''Return the rotation matrix to convert vector from global to
+        local frames'''
+        return     dot(rotyM(self.rotys(s)[0]),dot(rotxM(self.rotxs(s)[0]),\
+                                                    rotzM(self.rotzs(s)[0])))
+    
+    def _getRotMatrixLocalToGlobal(self,s):
+        
+        '''Return the rotation matrix to convert vector from global to
+        local frames'''
+        return transpose(dot(rotyM(self.rotys(s)[0]),dot(rotxM(self.rotxs(s)[0]),\
+                                                    rotzM(self.rotzs(s)[0]))))
---
+++ examples/sweep_opt/sweep_opt.py
@@ -0,0 +1,330 @@
+# =============================================================================
+# Standard Python modules
+# =============================================================================
+import os, sys, string, pdb, copy, time
+
+# =============================================================================
+# External Python modules
+# =============================================================================
+from numpy import linspace, cos, pi, hstack, zeros, ones, sqrt, imag, interp, \
+    array, real, reshape, meshgrid, dot, cross,shape,alltrue,loadtxt,max,min,\
+    take, put
+
+# =============================================================================
+# Extension modules
+# =============================================================================
+
+from mdo_import_helper import *
+import warnings
+warnings.filterwarnings("ignore")
+
+# Import PETSc so it is initialized on ALL Procs
+import petsc4py 
+petsc4py.init(args=sys.argv)
+
+# MDO_Lab Imports
+exec(import_modules('pyGeo','pyBlock','pySpline','geo_utils','mpi4py','pyLayout2'))
+exec(import_modules('RefAxis'))
+exec(import_modules('pyAero_problem','pyAero_reference','pyAero_geometry'))
+exec(import_modules('pyAeroStruct'))
+exec(import_modules('pyOpt_Optimization','pySNOPT_MPI'))
+     
+if 'complex' in sys.argv:
+    exec(import_modules('pyAero_flow_C'))
+    exec(import_modules('pySUMB_C','MultiblockMesh_C'))
+    exec(import_modules('TACS_cs','functions_cs','constitutive_cs','elements_cs'))
+    TACS = TACS_cs
+    functions = functions_cs
+    constitutive = constitutive_cs
+    elements = elements_cs
+    SUMB = SUMB_C
+    MBMesh = MBMesh_C
+    complex = True
+else:
+    # Real Imports
+    exec(import_modules('pyAero_flow'))
+    exec(import_modules('pySUMB','MultiblockMesh'))
+    exec(import_modules('TACS','functions','constitutive','elements'))
+    complex = False
+# end if
+
+mpiPrint('------- Sweep Example ----------------')
+
+# ================================================================
+#                   Grid File
+grid_file = 'sweep_debug'
+#grid_file = 'sweep_400k'
+#
+# ================================================================
+#        Set the number of processors for Aero and Structures
+npAero = 3
+npStruct =1
+comm,flags,cumGroups = createGroups([npAero,npStruct],noSplit=False)
+aeroID = 0
+structID = 1
+
+# ================================================================
+#               Set Options for each solver
+#
+aeroOptions={'reinitialize':False,'CFL':.80,
+             'L2Convergence':1e-7,'L2ConvergenceRel':1e-7,
+             'MGCycle':'4w','MetricConversion':1.0,'sol_restart':'no',
+             'printIterations':False,'printSolTime':False,'writeSolution':False,
+             'Dissipation Coefficients':[0.30,0.020],
+             'Dissipation Scaling Exponent':0.125,
+             'Approx PC': 'yes',
+             'Adjoint solver type':'GMRES',
+             'adjoint relative tolerance':1e-8,
+             'adjoint absolute tolerance':1e-16,
+             'adjoint max iterations': 500,
+             'adjoint restart iteration' : 100,
+             'adjoint monitor step': 20,
+             'dissipation lumping parameter':6.0,
+             'Preconditioner Side':'LEFT',
+             'Matrix Ordering': 'NestedDissection',
+             'Global Preconditioner Type': 'Additive Schwartz',
+             'Local Preconditioner Type' : 'ILU',
+             'ILU Fill Levels': 2,
+             'ASM Overlap':6,
+             'Reference Pressure':24540.0,
+             'Reference Density':.38857
+             }            
+
+structOptions={'PCFillLevel':7,
+               'PCFillRatio':9.5,
+               'msub':10,
+               'subSpaceSize':140,
+               'nRestarts':15,
+               'flexible':1,
+               'L2Convergence':1e-11,
+               'L2ConvergenceRel':1e-3,
+               'useMonitor':False,
+               'monitorFrequency':1,
+               'filename':'wing_box',
+               'restart':False}
+
+mdOptions = {'relTol':1e-5,
+             'writeIterationVolSolutionCFD':False,
+             'writeIterationSurfSolutionCFD':False,
+             'writeIterationSolutionFEA':False,
+             'writeVolSolutionCFD':False,
+             'writeSurfSolutionCFD':True,
+             'writeSolutionFEA':True,
+             'writeMDConvergence':True,
+             'MDConvergenceFile':'mdconverg.txt',
+             'beta0':0.25,
+             'CFDIter':750,
+             }
+
+meshOptions = {'warpType':'solid',
+               'solidWarpType':'n', # or 'topo'
+               'n':3,
+               'sym':'xy'
+               }
+
+# Snopt Opt Options
+optOptions = {'Major feasibility tolerance':1.0e-3,
+              'Major optimality tolerance':1.0e-3, 
+              'Minor feasibility tolerance':1.0e-2,
+              'Verify level':0,			
+              'Scale tolerance':0.9,		
+              'Major iterations limit':50,
+              'Minor iterations limit':500,
+              'Major step limit':1.0,
+              'Derivative level':0,	     
+              'Nonderivative linesearch':None,
+              'Function precision':1.0e-5,
+              'Difference interval':.003, 	# Function precision^(1/2)
+              'Central difference interval':.02# Function precision^(1/3)
+}
+
+# ================================================================
+#               Setup the Free-Form Deformation Volume
+FFD = pyBlock.pyBlock('plot3d',file_name='ffd_linear.fmt',file_type='ascii',order='f')
+FFD.doConnectivity('ffd_connectivity.con')
+FFD.fitGlobal()
+FFD.coef *= 2.33 
+FFD._updateVolumeCoef()
+
+# Setup curves for ref_axis
+z = array([0,5.1])*2.33
+y = array([0,0])*2.33
+x = array([.5,.5])*2.33
+c1 = pySpline.curve(x=x,y=y,z=z,k=2)
+ref_axis = RefAxis.RefAxis([c1],FFD.coef,rot_type=5)
+
+# ================================================================
+# Set Design Variable Functions
+
+def sweep(val,ref_axis):
+    
+    # Do a proper rotation sweep, but still shear the sections
+    
+    # Extract coef for 0-th ref axis
+    C = zeros((len(ref_axis.topo.l_index[0]),3))
+    C[:,0] = take(ref_axis.coef[:,0],ref_axis.topo.l_index[0])
+    C[:,1] = take(ref_axis.coef[:,1],ref_axis.topo.l_index[0])
+    C[:,2] = take(ref_axis.coef[:,2],ref_axis.topo.l_index[0])
+
+    M = rotyM(val)
+    C[1] = C[0] + dot(M,C[1]-C[0])
+
+    # Now set the y-rotations
+    ref_axis.rot_y[0].coef[1] = val
+
+    # Scale the Root
+    ref_axis.scale_x[0].coef[0] = ref_axis.scale_x0[0].coef[0] / cos(val*pi/180)
+
+
+    # Reset the coef
+    put(ref_axis.coef[:,0],ref_axis.topo.l_index[0],C[:,0])
+    put(ref_axis.coef[:,1],ref_axis.topo.l_index[0],C[:,1])
+    put(ref_axis.coef[:,2],ref_axis.topo.l_index[0],C[:,2])
+    
+    return 
+
+def twist(val,ref_axis):
+
+    ref_axis.rot_z[0].coef[1] = val
+    
+    return
+
+# Fifth: Add design variables (global) to the ref axis object
+
+ref_axis.addGeoDVGlobal('sweep',0,0,40,sweep)
+ref_axis.addGeoDVGlobal('twist',0,-10,10,twist)
+idg = ref_axis.DV_namesGlobal #NOTE: This is constant (idg -> id global
+
+# =====================================================
+#        Setup Aero-Solver and multiblock Mesh
+
+if flags[aeroID] == True:
+    flow = Flow(name='Base Case',mach=0.8,alpha=0,beta=0.0,liftIndex=2)
+    ref = Reference('Baseline Reference',5.0,5.0,1.0) #area,span,chord 
+    geom = Geometry
+    aeroProblem = AeroProblem(name='AeroStruct Test',geom=geom,flow_set=flow,ref_set=ref)
+    CFDsolver = SUMB(comm=comm,init_petsc=False,options=aeroOptions)
+    mesh = MBMesh(grid_file,comm,meshOptions=meshOptions)
+    mesh.addFamilyGroup("wing")
+    mesh.warpMesh()
+    CFDsolver.initialize(aeroProblem,'steady',grid_file)
+    CFDsolver.interface.Mesh.initializeExternalWarping(mesh.getGridDOF())
+    structure = None
+
+    coords = mesh.getSurfaceCoordinatesLocal('wing')
+    FFD.embedVolume(coords)
+    
+# end if
+
+# =====================================================
+#                Setup Struct Solver
+
+if flags[structID]:
+    execfile('setup_structure.py') # structure comes out of this
+    aeroProblem = None
+    CFDsolver = None
+    mesh = None
+    mass = structure.evalFunction(mass_func)
+    ks   = structure.evalFunction(ks_func)
+    print 'Mass is:',mass
+
+    Nnodes = structure.getNumVariables()
+    coords = empty(3*Nnodes,'d')
+    structure.getNodes(coords)
+    coords = coords.reshape((Nnodes,3))
+    FFD.embedVolume(coords)
+# end if
+
+
+# =====================================================
+#            Setup Aero-Struct Solver
+
+AS = AeroStruct(MPI.COMM_WORLD,comm,flags,aeroOptions=aeroOptions, 
+                structOptions=structOptions,mdOptions=mdOptions,complex=complex)
+
+AS.initialize(aeroProblem,CFDsolver,structure,mesh,'wing')
+# =====================================================
+#                Objective Function
+
+def fun_obj(x):
+
+    # =====================================================
+    #        Set DV's
+    if MPI.COMM_WORLD.rank == 0:
+        print 'DVs are:',x
+    # Set up the Flow and aeroProblem again 
+    flow = Flow(name='Base Case',mach=0.8,alpha=x[0],beta=0.0,liftIndex=2)
+    ref = Reference('Baseline Reference',5.0,5.0,1.0) #area,span,chord 
+    geom = Geometry
+    AS.aero_problem = AeroProblem(name='AeroStruct Test',geom=geom,flow_set=flow,ref_set=ref)
+
+    # Set Sweep and Twist
+    ref_axis.DV_listGlobal[idg['sweep']].value = x[1]
+    ref_axis.DV_listGlobal[idg['twist']].value = x[2]
+
+    # Update FFD
+    FFD.coef = ref_axis.update()
+    FFD._updateVolumeCoef()
+
+    # Update CFD Grid
+    if flags[aeroID] == True:
+        mesh.setSurfaceCoordinatesLocal('wing',FFD.getVolumePoints(0))
+        mesh.warpMesh()
+        CFDsolver.interface.Mesh.setGrid(mesh.getGrid()) 
+
+    # Update Structure Grid
+    if flags[structID] == True:
+        structure.setNodes(FFD.getVolumePoints(0).flatten())
+
+    # Solve the aerostructural Problem
+    try:
+        Force = AS.solve(nMDiterations=1)
+        M = rotzM(-x[0])
+        Force = dot(M,Force)
+    
+        Lift = Force[1]
+        Drag = Force[0]
+
+        f_obj = Drag
+        f_con = array([Lift])
+    
+        fail = 0
+    except:
+        fail = 1
+        f_obj = 0.0
+        f_con = [0.0]
+
+
+    if MPI.COMM_WORLD.rank == 0:
+        print 'x',x
+        print 'f_obj:',f_obj
+        print 'f_con:',f_con
+
+
+    return f_obj,f_con,fail
+
+
+
+# =====================================================
+# Set-up Optimization Problem
+
+opt_prob = Optimization('Swept Wing Optimization',fun_obj)
+
+# Add variables
+opt_prob.addVar('alpha',value=-2.,lower=-2.,upper=4.)
+opt_prob.addVar('sweep',value=5.,lower=0.,upper=45.)
+opt_prob.addVar('twist',value=0.,lower=-5.,upper=5.)
+
+# Add Constraints
+opt_prob.addCon('lift', type='i', lower=95870.0, upper=1000000.0)
+
+# Add Objective 
+opt_prob.addObj('Drag')
+
+# Make Instance of Optimizer
+snopt = SNOPT(options=optOptions)
+
+# Run optimization
+snopt(opt_prob)
+#fun_obj([0,30,0])
+#fun_obj([0,30,0])
