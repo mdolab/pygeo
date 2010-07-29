63e3fd3f85b304c5e777b48e85f27aae08e3aa09
Gaetan Kenway <gaetank@gmail.com>
2010-07-29 18:54:47 +0000

    update to sweep_opt example

--- examples/sweep_opt/sweep_opt.py
+++ examples/sweep_opt/sweep_opt.py
@@ -8,7 +8,7 @@ import os, sys, string, pdb, copy, time
 # =============================================================================
 from numpy import linspace, cos, pi, hstack, zeros, ones, sqrt, imag, interp, \
     array, real, reshape, meshgrid, dot, cross,shape,alltrue,loadtxt,max,min,\
-    take, put
+    take, put,log
 
 # =============================================================================
 # Extension modules
@@ -52,11 +52,11 @@ mpiPrint('------- Sweep Example ----------------')
 
 # ================================================================
 #                   Grid File
-grid_file = 'sweep_debug'
-#grid_file = 'sweep_400k'
+grid_file = 'sweep_debug2'
+#grid_file = 'sweep_400k2'
 #
 # ================================================================
-#        Set the number of processors for Aero and Structures
+#        Set the number oBf processors for Aero and Structures
 npAero = MPI.COMM_WORLD.size -1 
 npStruct =1
 comm,flags,cumGroups = createGroups([npAero,npStruct],noSplit=False)
@@ -67,12 +67,13 @@ structID = 1
 # ================================================================
 #               Set Options for each solver
 #
-aeroOptions={'reinitialize':False,'CFL':.80,
-             'L2Convergence':1e-6,'L2ConvergenceRel':1e-6,
+aeroOptions={'reinitialize':False,'CFL':1.45, # 1.5 for large grid
+             'L2Convergence':1e-6,'L2ConvergenceRel':1e-1,
              'MGCycle':'4w','MetricConversion':1.0,'sol_restart':'no',
+             'OutputDir':'/scratch/kenway/',
              'printIterations':False,'printSolTime':False,'writeSolution':False,
-             'Dissipation Coefficients':[0.30,0.020],
-             'Dissipation Scaling Exponent':0.125,
+             'Dissipation Coefficients':[0.25,0.016], 
+             'Dissipation Scaling Exponent':0.125, # .25 for large grid
              'Approx PC': 'yes',
              'Adjoint solver type':'GMRES',
              'adjoint relative tolerance':1e-8,
@@ -101,7 +102,7 @@ structOptions={'PCFillLevel':7,
                'L2ConvergenceRel':1e-3,
                'useMonitor':False,
                'monitorFrequency':1,
-               'filename':'wing_box',
+               'filename':'/scratch/kenway/wing_box',
                'restart':False}
 
 mdOptions = {'relTol':1e-5,
@@ -109,12 +110,12 @@ mdOptions = {'relTol':1e-5,
              'writeIterationSurfSolutionCFD':False,
              'writeIterationSolutionFEA':False,
              'writeVolSolutionCFD':False,
-             'writeSurfSolutionCFD':True,
+             'writeSurfSolutionCFD':False,
              'writeSolutionFEA':False,
              'writeMDConvergence':False,
              'MDConvergenceFile':'mdconverg.txt',
-             'beta0':0.25,
-             'CFDIter':750,
+             'beta0':.5,
+             'CFDIter':2500,
              }
 
 meshOptions = {'warpType':'solid',
@@ -125,18 +126,19 @@ meshOptions = {'warpType':'solid',
 
 # Snopt Opt Options
 optOptions = {'Major feasibility tolerance':1.0e-3,
-              'Major optimality tolerance':1.0e-3, 
-              'Minor feasibility tolerance':1.0e-2,
-              'Verify level':0,			
-              'Scale tolerance':0.9,		
+              'Major optimality tolerance':1.00e-3, 
+              'Verify level':-1,			
               'Major iterations limit':50,
               'Minor iterations limit':500,
-              'Major step limit':1.0,
+              'Major step limit':.1,
               'Derivative level':0,	     
               'Nonderivative linesearch':None,
               'Function precision':1.0e-5,
-              'Difference interval':.003, 	# Function precision^(1/2)
-              'Central difference interval':.02# Function precision^(1/3)
+              #'Difference interval':.003, 	# Function precision^(1/2)
+              #'Central difference interval':.02,# Function precision^(1/3)
+              'Print file':'/scratch/kenway/SNOPT_print.out',
+              'Summary file':'/scratch/kenway/SNOPT_summary.out',
+              'Problem Type':'Maximize'
 }
 
 # ================================================================
@@ -153,7 +155,9 @@ y = array([0,0])*2.33
 x = array([.5,.5])*2.33
 c1 = pySpline.curve(x=x,y=y,z=z,k=2)
 ref_axis = RefAxis.RefAxis([c1],FFD.coef,rot_type=5)
-
+Area_ref = 5.0*2.33*2.33
+Span_ref = 5.0*2.33
+Chord_ref = 1.0*2.33
 # ================================================================
 # Set Design Variable Functions
 
@@ -167,14 +171,18 @@ def sweep(val,ref_axis):
     C[:,1] = take(ref_axis.coef0[:,1],ref_axis.topo.l_index[0])
     C[:,2] = take(ref_axis.coef0[:,2],ref_axis.topo.l_index[0])
 
-    M = rotyM(val)
-    C[1] = C[0] + dot(M,C[1]-C[0])
+    d0 = norm(C[1]-C[0])
+    C[1][0] += d0*tan(val*pi/180)
+    
+    #print 'C[1]:',C[1]
+    #M = rotyM(val)
+    #C[1] = C[0] + dot(M,C[1]-C[0])
 
     # Now set the y-rotations
-    ref_axis.rot_y[0].coef[1] = val
+    #ref_axis.rot_y[0].coef[1] = val
 
     # Scale the Root
-    ref_axis.scale_x[0].coef[0] = ref_axis.scale_x0[0].coef[0] / cos(val*pi/180)
+    #ref_axis.scale_x[0].coef[0] = ref_axis.scale_x0[0].coef[0] / cos(val*pi/180)
 
 
     # Reset the coef
@@ -201,7 +209,7 @@ idg = ref_axis.DV_namesGlobal #NOTE: This is constant (idg -> id global
 
 if flags[aeroID] == True:
     flow = Flow(name='Base Case',mach=0.8,alpha=0,beta=0.0,liftIndex=2)
-    ref = Reference('Baseline Reference',5.0,5.0,1.0) #area,span,chord 
+    ref = Reference('Baseline Reference',Area_ref,Span_ref,Chord_ref) #area,span,chord 
     geom = Geometry
     aeroProblem = AeroProblem(name='AeroStruct Test',geom=geom,flow_set=flow,ref_set=ref)
     CFDsolver = SUMB(comm=comm,init_petsc=False,options=aeroOptions)
@@ -252,21 +260,19 @@ def fun_obj(x):
     #        Set DV's
 
     # Sclale DV's back to range
-    alpha = x[0]*6-3
-    sweep = x[1]*45 + 0
-    twist = x[2]*20 - 10
-
-    if MPI.COMM_WORLD.rank == 0:
-        print 'DVs are:'
-        print 'x',x
-        print 'alpha:',alpha
-        print 'sweep:',sweep
-        print 'twist:',twist
-    # Set up the Flow and aeroProblem again 
-    flow = Flow(name='Base Case',mach=0.8,alpha=alpha,beta=0.0,liftIndex=2)
-    ref = Reference('Baseline Reference',5.0,5.0,1.0) #area,span,chord 
-    geom = Geometry
-    AS.aero_problem = AeroProblem(name='AeroStruct Test',geom=geom,flow_set=flow,ref_set=ref)
+    alpha = x[0]*8-4
+    sweep = x[1]*45
+    twist = 0#x[2]*10 - 5
+    thick1 = .03
+    thick2 = x[2]
+    r = MPI.COMM_WORLD.rank
+    print 'DVs are:'
+    print 'x',r,x
+    print 'alpha:',r,alpha
+    print 'sweep:',r,sweep
+    print 'twist:',r,twist
+    print 'thick1:',r,thick1
+    print 'thick2:',r,thick2
 
     # Set Sweep and Twist
     ref_axis.DV_listGlobal[idg['sweep']].value = sweep
@@ -276,42 +282,87 @@ def fun_obj(x):
     FFD.coef = ref_axis.update()
     FFD._updateVolumeCoef()
 
-    # Update CFD Grid
-    if flags[aeroID] == True:
+    # Update Structures
+    if flags[structID]:
+        structure.setDesignVars([thick1,thick2])
+        structure.setNodes(FFD.getVolumePoints(0).flatten())
+    # Update Aero
+    if flags[aeroID]: 
+        # Set up the Flow and aeroProblem again 
+        flow = Flow(name='Base Case',mach=0.8,alpha=alpha,beta=0.0,liftIndex=2)
+        ref = Reference('Baseline Reference',Area_ref,Span_ref,Chord_ref) #area,span,chord 
+        geom = Geometry
+        AS.aero_problem = AeroProblem(name='AeroStruct Test',geom=geom,flow_set=flow,ref_set=ref)
+
         mesh.setSurfaceCoordinatesLocal('wing',FFD.getVolumePoints(0))
         mesh.warpMesh()
         CFDsolver.interface.Mesh.setGrid(mesh.getGrid()) 
-
-    # Update Structure Grid
-    if flags[structID] == True:
-        structure.setNodes(FFD.getVolumePoints(0).flatten())
+    # end if
 
     # Solve the aerostructural Problem
-    try:
-        Force = AS.solve(nMDiterations=1)
-        M = rotzM(-alpha)
-        Force = dot(M,Force)
-    
-        Lift = Force[1]
-        Drag = Force[0]
+    Force,fail = AS.solve(nMDiterations=20)
 
-        f_obj = Drag/1000
-        f_con = array([Lift])/95870.0
-    
-        fail = 0
-    except:
+    if fail:
         fail = 1
         f_obj = 0.0
-        f_con = [0.0]
+        f_con = [0.0,0.0]
 
+        return f_obj,f_con,fail
+    # end try
+
+    if flags[structID]: #bcast mass and ks 
+        mass = structure.evalFunction(mass_func)
+        ks   = structure.evalFunction(ks_func)
+    else:
+        mass = None
+        ks = None
+    # end if
+
+    mass = MPI.COMM_WORLD.bcast(mass,root=AS.sroot)
+    ks   = MPI.COMM_WORLD.bcast(ks,root=AS.sroot)
+    
+    M = rotzM(-alpha)
+    Force = dot(M,Force)
+    
+    Lift = Force[1]
+    Drag = Force[0]
+    f_con = zeros([2],'d')
+
+    #f_obj = (Lift/Drag)*log(W1/W2)
+    # OEW = 11791 kg
+    # MTOW = 19,500 kg
+    # Wing Weight = 2358 kg (nominal 20% of OEW)
+    # Sturct Weight = 9432 kg
+    # Passenger = 5245 kg
+    # Fuel = 2464 kg
+
+    # W1 = MTOW = OEW + Pssenger + Fuel
+    # W1 = MTOW = Struct_weight + wing_weight + Passenger + Fuel
+    W1 = 2358 + mass*2 + 5245 + 2464
+
+    # W2 = Final Weight = Struct_weight + wing_weight + Passenger
+    W2 = 2358 + mass*2 + 5245
+    
+    f_obj = Lift/Drag*log(W1/W2)
+
+    # Lift Constraint:
+    # Lift = Weight = MTOW = W1 = Stuct_wiehgt + wing_weight +passenger + fuel
+    # Scale my original MTOW
+    f_con[0] = ((2*Lift/9.81) - (9432 + mass*2 + 5245 + 2464) )/19500
+    f_con[1] = ks
+
+    #f_obj = Drag
+    # f_obj = Drag/1000
+    #f_con = array([Lift])/95870.0
+   
+    fail = 0
 
     if MPI.COMM_WORLD.rank == 0:
+        print 'mass:',mass
         print 'f_obj:',f_obj
         print 'f_con:',f_con
 
     return f_obj,f_con,fail
-
-
 
 # =====================================================
 # Set-up Optimization Problem
@@ -319,18 +370,40 @@ def fun_obj(x):
 opt_prob = Optimization('Swept Wing Optimization',fun_obj)
 
 # Add variables
-opt_prob.addVar('alpha',value=0.5,lower=0.,upper=1.)
-opt_prob.addVar('sweep',value=.1,lower=0.,upper=1.)
-opt_prob.addVar('twist',value=.5,lower=0.,upper=1.)
-
+opt_prob.addVar('alpha',value=0.453098,lower=0.,upper=1.)
+opt_prob.addVar('sweep',value=.25,lower=0.,upper=1.)
+#opt_prob.addVar('twist',value=.5,lower=0.,upper=1.)
+#opt_prob.addVar('thick1',value=.05,lower=.005,upper=1)
+opt_prob.addVar('thick2',value=0.01,lower=0.005,upper=1)
 # Add Constraints
-opt_prob.addCon('lift', type='i', lower=1.0, upper=1.01)
-
+opt_prob.addCon('lift', type='e', value=0.0)
+opt_prob.addCon('KS',type='i',lower=0.0,upper=10.0)
 # Add Objective 
-opt_prob.addObj('Drag')
+opt_prob.addObj('Range')
 
 # Make Instance of Optimizer
-snopt = SNOPT(options=optOptions)
+#snopt = SNOPT(options=optOptions)
 
 # Run optimization
-snopt(opt_prob)
+#print opt_prob
+#snopt(opt_prob)
+#r = MPI.COMM_WORLD.rank
+#print 'rank:',r,opt_prob
+
+fun_obj([.5,0,.005])
+fun_obj([.501,0,.00500])
+fun_obj([.50,0.001,.00500])
+fun_obj([.50,0.000,.00510])
+# if MPI.COMM_WORLD.rank == 0:
+#     f = open('/scratch/kenway/alpha_sweep.txt','w')
+#     f.write('Lift           Drag               KS\n')
+# for i in xrange(10):
+#     alpha_dv = i/10.0
+#     f_obj,f_con,fail = fun_obj([alpha_dv,0.0,.08,.02])
+#     if MPI.COMM_WORLD.rank == 0:
+#         f.write('%20.15e %20.15e %20.15e \n'%(f_con[0],f_obj,f_con[1]))
+#     # end if
+# # end for
+# if MPI.COMM_WORLD.rank == 0:
+#     f.close()
+                
