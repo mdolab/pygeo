13bf0e26f8f9ea1619e225d0ce127f59feb7cf13
Gaetan Kenway <gaetank@gmail.com>
2013-05-20 16:00:07 -0400

    fixed a bug in pygeo surfacing when more than two segments are present. Removed old examples that were outdated and would not. There are two examples that have been updated and demonstrate different types of surface creation.

---
+++ examples/c172_wing/c172.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+# =============================================================================
+# Standard Python modules
+# =============================================================================
+
+# =============================================================================
+# External Python modules
+# =============================================================================
+import numpy
+
+# =============================================================================
+# Extension modules
+# =============================================================================
+from mdo_import_helper import import_modules
+exec(import_modules('pyGeo'))
+
+# ==============================================================================
+# Start of Script
+# ==============================================================================
+naf = 3
+airfoil_list = ['naca2412.dat','naca2412.dat','naca2412.dat']
+Nctl = 13
+chord = [1.67,1.67,1.18]
+x = [0,0,.125*1.18]
+y = [0,0,0]
+z = [0,2.5,10.58/2]
+rot_x = [0,0,0]
+rot_y = [0,0,0]
+rot_z = [0,0,2] 
+offset = numpy.zeros((naf,2))
+
+# There are several examples that follow showing many of the different
+# combinations of tip/trailing edge options that are available.
+
+# --------- Sharp Trailing Edge / Rounded Tip -------
+wing = pyGeo.pyGeo('lifting_surface',
+                   xsections=airfoil_list,
+                   scale=chord, offset=offset, x=x, y=y, z=z,
+                   rot_x=rot_x, rot_y=rot_y, rot_z=rot_z,
+                   k_span=2, tip='rounded')
+
+wing.writeTecplot('c172_sharp_te_rounded_tip.dat')
+wing.writeIGES('c172_sharp_te_rounded_tip.igs')
+
+# --------- Sharp Trailing Edge / Pinched Tip -------
+wing = pyGeo.pyGeo('lifting_surface',
+                   xsections=airfoil_list,
+                   scale=chord, offset=offset, x=x, y=y, z=z,
+                   rot_x=rot_x, rot_y=rot_y, rot_z=rot_z,
+                   k_span=2, tip='pinched')
+
+wing.writeTecplot('c172_sharp_te_pinched_tip.dat')
+wing.writeIGES('c172_sharp_te_pinched_tip.igs')
+
+# --------- Sharp Trailing Edge / Rounded Tip with Fitting -------
+# This option shouldn't be used except to match previously generated
+# geometries
+wing = pyGeo.pyGeo('lifting_surface',
+                   xsections=airfoil_list, Nctl=29,
+                   scale=chord, offset=offset, x=x, y=y, z=z,
+                   rot_x=rot_x, rot_y=rot_y, rot_z=rot_z,
+                   k_span=2, tip='rounded')
+
+wing.writeTecplot('c172_sharp_te_rounded_tip_fitted.dat')
+wing.writeIGES('c172_sharp_te_rounded_tip_fitted.igs')
+
+# --------- Blunt Trailing (Flat) / Rounded Tip ------- 
+
+# This is the normal way of producing blunt TE geometries. The
+# thickness of the trailing edge is specified with 'te_height', either
+# a constant value or an array of length naf. This is in physical
+# units. Alternatively, 'te_height_scaled' can be specified to have a
+# scaled thickness. This option is specified as a fraction of initial
+# chord, so te_height_scaled=0.002 will give a 0.2% trailing edge
+# thickness
+wing = pyGeo.pyGeo('lifting_surface',
+                   xsections=airfoil_list,
+                   scale=chord, offset=offset, x=x, y=y, z=z,
+                   rot_x=rot_x, rot_y=rot_y, rot_z=rot_z,
+                   blunt_te=True, te_height_scaled=0.002,
+                   k_span=2, tip='rounded')
+
+wing.writeTecplot('c172_blunt_te_rounded_tip.dat')
+wing.writeIGES('c172_blunt_te_rounded_tip.igs')
+
+# --------- Blunt Trailing (Rounded) / Rounded Tip ------- 
+
+# Alternative way of producing rounded trailing edges that can be easier
+# to mesh and extrude with pyHyp. 
+wing = pyGeo.pyGeo('lifting_surface',
+                   xsections=airfoil_list,
+                   scale=chord, offset=offset, x=x, y=y, z=z,
+                   rot_x=rot_x, rot_y=rot_y, rot_z=rot_z,
+                   blunt_te=True, rounded_te=True, te_height_scaled=0.002,
+                   k_span=2, tip='rounded')
+
+wing.writeTecplot('c172_rounded_te_rounded_tip.dat')
+wing.writeIGES('c172_rounded_te_rounded_tip.igs')
