a26075342d5369797f9e31ffd1a92201085cdf50
Gaetan Kenway <gaetank@gmail.com>
2014-03-01 13:36:34 -0500

    More general clean up. Modification made to match those in pySpline.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,2 @@
 from . import geo_utils, pyNetwork, pyGeo, pyBlock
 from . import DVConstraints, DVGeometry
-
-__all__ = ['pyGeo', 'pyBlock', 'pyNetwork', 'DVGeometry', 'DVConstraints',
-           'geo_utils']
