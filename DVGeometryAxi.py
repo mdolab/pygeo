4c45aa97e2a58b07fd16bdcb1c3fcb2f0b888ad3
Justin Gray <justin.s.gray@gmail.com>
2016-09-28 19:57:06 -0400

    derivs work. Just need to finish the test now

--- DVGeometryAxi.py
+++ DVGeometryAxi.py
@@ -1,6 +1,7 @@
-# from __future__ import print_function
+from __future__ import print_function
 
 import numpy as np 
+from scipy import sparse
 
 try:
     from collections import OrderedDict
@@ -15,6 +16,7 @@ from . import DVGeometry
 
 AXES_2_IDX = {"x": 0, "y": 1, "z": 2}
 AXES = set(['x', 'y', 'z'])
+
 
 class _AxiTransform(object): 
     """Collapses a set of cartesian coordiantes into a single plane to allow 
@@ -47,35 +49,79 @@ class _AxiTransform(object):
         self.beta_idx = AXES_2_IDX[self.c_plane[1]]
         self.gamma_idx = AXES_2_IDX[AXES.difference(set(self.c_plane)).pop()]  # which ever one isn't in the c_plane tuple!
 
-        self.pts = pts.copy()
+        # self.pts = pts.copy()
 
         # re-order the columns to a consistent alpha, beta, gamma frame
-        self.alpha = self.pts[:, self.alpha_idx]  
-        self.beta = self.pts[:, self.beta_idx] - center[self.beta_idx]
-        self.gamma = self.pts[:, self.gamma_idx] - center[self.gamma_idx]
+        alpha = pts[:, self.alpha_idx]  
+        beta = pts[:, self.beta_idx] - center[self.beta_idx]
+        gamma = pts[:, self.gamma_idx] - center[self.gamma_idx]
 
-        self.radii = np.sqrt(self.beta**2 + self.gamma**2)
-        self.thetas = np.arctan2(self.gamma, self.beta)
+        self.radii = np.sqrt(beta**2 + gamma**2)
+        self.thetas = np.arctan2(gamma, beta)
+
+        self.cos_thetas = np.cos(self.thetas)
+        self.sin_thetas = np.sin(self.thetas)
+
+        # transformation jacobian to account for the axisymmetric transformation
+        n_pts = len(pts)
+
+        row = np.empty(3*n_pts, dtype="int")
+        col = np.empty(3*n_pts, dtype="int")
+        data = np.empty(3*n_pts)
+        # for j in range(n_pts): 
+        #     # Pt_j_x
+        #     row[j] = 0 + 3*j
+        #     col[j] = self.alpha_idx + 3*j
+        #     data[j] = 1.
+        #     # Pt_j_y
+        #     row[j+1] = 1 + 3*j
+        #     col[j+1] = self.beta_idx + 3*j
+        #     data[j+1] = self.sin_thetas[j]
+        #     # Pt_j_z
+        #     row[j+2] = 2 + 3*j
+        #     col[j+2] = self.beta_idx + 3*j
+        #     data[j+2] = self.cos_thetas[j]
+
+        # vectorized
+        idx = 3*np.arange(n_pts, dtype="int")
+        row[0::3] = idx
+        row[1::3] = 1+idx
+        row[2::3] = 2+idx
+
+        col[0::3] = self.alpha_idx + idx
+        col[1::3] = self.beta_idx + idx
+        col[2::3] = self.beta_idx + idx
+
+        data[0::3] = 1.
+        data[1::3] = self.sin_thetas
+        data[2::3] = self.cos_thetas
+
+        # derivative of the Cartesian points w.r.t the collapsed axi-symmetric points
+        self.dPtCdPtA = sparse.coo_matrix((data, (row, col)), 
+                                          shape=(3*n_pts, 3*n_pts)).tocsr()
 
         # points collapsed into the prescribed plane
         # self.c_pts_axi = np.vstack((self.alpha, self.radii, np.zeros(self.n_points))).T
-        self.c_pts = np.empty(self.pts.shape)
-        self.c_pts[:, 0] = self.alpha 
+        self.c_pts = np.empty((self.n_points, 3))
+        self.c_pts[:, 0] = alpha 
         self.c_pts[:, self.beta_idx] = self.radii
-        self.c_pts[:, self.gamma_idx] = 0.
+        self.c_pts[:, self.gamma_idx] = 0.  # no need to store zeros
 
     def expand(self, new_c_pts): 
         """given new collapsed points, re-expands them into physical space"""
 
         self.c_pts = new_c_pts 
-        self.pts[:, self.alpha_idx] = new_c_pts[:, 0] 
-        self.pts[:, self.beta_idx] = new_c_pts[:, self.beta_idx]*np.cos(self.thetas) + self.center[self.beta_idx]
-        self.pts[:, self.gamma_idx] = new_c_pts[:, self.beta_idx]*np.sin(self.thetas) + self.center[self.gamma_idx]
+        pts = np.empty((self.n_points, 3))
+        pts[:, self.alpha_idx] = new_c_pts[:, 0] 
 
-        return self.pts
+        new_rads = new_c_pts[:, self.beta_idx]
+        pts[:, self.beta_idx] = new_rads*self.cos_thetas + self.center[self.beta_idx]
+        pts[:, self.gamma_idx] = new_rads*self.sin_thetas + self.center[self.gamma_idx]
+
+        return pts
+
 
 class DVGeometryAxi(DVGeometry):
-
 
     def __init__(self, fileName, complex=False, child=False, *args, **kwargs): 
 
@@ -130,5 +176,28 @@ class DVGeometryAxi(DVGeometry):
         # coords = new_c_pts
 
         return coords 
+
+    def computeTotalJacobian(self, ptSetName, config=None): 
+        """ compute the total point jacobian in CSR format since we
+        need this for TACS"""
+
+        super(DVGeometryAxi, self).computeTotalJacobian(ptSetName, config)
+
+        xform = self.axiTransforms[ptSetName]
+
+        self.JT[ptSetName] = xform.dPtCdPtA.dot(self.JT[ptSetName].T).T
+
+
+    def computeTotalJacobianFD(self, ptSetName, config=None): 
+
+        super(DVGeometryAxi, self).computeTotalJacobianFD(ptSetName, config)
+
+        xform = self.axiTransforms[ptSetName]
+
+        self.JT[ptSetName] = xform.dPtCdPtA.dot(self.JT[ptSetName].T).T
+
+
+
+
 
 
