6e3b8da65a64ef92fd27d2ac4711b6121ff5819c
Anil Yildirim <anily@umich.edu>
2020-01-04 13:04:10 +0300

    first working version with twist op

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ from .pyBlock import pyBlock
 from .DVConstraints import DVConstraints
 from .DVGeometry import DVGeometry
 from .DVGeometryAxi import DVGeometryAxi
+from .om_dvgeo import OM_DVGEO
 try:
     from .DVGeometryVSP import DVGeometryVSP
 except ImportError:
