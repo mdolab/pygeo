d7d3e63992545a3ea2a28a3161b0afa0fd90b1ec
Anil Yildirim <anily@umich.edu>
2020-01-19 22:41:15 +0300

    removed openmdao dependency

--- __init__.py
+++ __init__.py
@@ -5,7 +5,10 @@ from .pyBlock import pyBlock
 from .DVConstraints import DVConstraints
 from .DVGeometry import DVGeometry
 from .DVGeometryAxi import DVGeometryAxi
-from .om_dvgeo import OM_DVGEO
+try:
+    from .om_dvgeo import OM_DVGEO
+except ImportError:
+    pass
 try:
     from .DVGeometryVSP import DVGeometryVSP
 except ImportError:
