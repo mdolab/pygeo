7db461ac1e26549481ec2ed683def3d4fce8ce1c
Marco Mangano <36549388+marcomangano@users.noreply.github.com>
2020-10-22 15:05:56 +0200

    Docs update (#45)

--- doc/conf.py
+++ doc/conf.py
@@ -1,11 +1,6 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+from sphinx_mdolab_theme.config import *
 
 # -- Path setup --------------------------------------------------------------
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -13,46 +8,17 @@
 import os
 import sys
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 project = u"pyGeo"
-copyright = u"2020, MDO Lab"
 
 # -- General configuration -----------------------------------------------------
-
-# If your documentation needs a minimal Sphinx version, state it here.
-# needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.imgmath", "sphinx.ext.viewcode", "numpydoc"]
+# Built-in Sphinx extensions are already contained in the imported variable
+# here we add external extensions, which must also be added to requirements.txt
+# so that RTD can import and use them
+extensions.extend(["numpydoc"])
 
 # mock import for autodoc
 autodoc_mock_imports = ["numpy", "mpi4py", "scipy", "pyspline"]
-
-numpydoc_show_class_members = False
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-exclude_patterns = ["_build"]
-
-# The master toctree document.
-master_doc = "index"
-
-# -- Options for HTML output ---------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
