98f90b0a73de6721af7af1b12f12f53b01d68e39
Bernardo Pacini <bpacini@umich.edu>
2021-03-17 14:28:10 -0400

    Deleting large input files from repo and adding get-input-file script. Input files added to UMich AFS.

---
+++ input_files/get-input-files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# this file will download the input files for pyGeo regression tests
+# and examples and extract them to the right place.
+
+DIR=$(dirname $0)
+wget -O $DIR/input_files.tar.gz http://umich.edu/~mdolaboratory/repo_files/pyGeo/pyGeo_input_files.tar.gz
+tar -xzf $DIR/input_files.tar.gz -C $DIR/../
