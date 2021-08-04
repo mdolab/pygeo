67bb13ea08cb8cc809180ef97e3cce338ed7733b
hajdik <hajdik@umich.edu>
2021-08-04 14:09:03 -0400

    added __init__ files

---
+++ pygeo/DVGeometry/__init__.py
@@ -0,0 +1,7 @@
+# =============================================================================
+# Classes for use in DVGeometry
+# =============================================================================
+
+# This __init__ file imports every methods in pygeo/DVGeometry
+from .globalDVs import *
+from .localDVs import *
