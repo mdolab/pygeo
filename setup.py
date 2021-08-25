078683db93e9cb23d199335735ddd726cb1298b1
Neil Wu <602725+nwu63@users.noreply.github.com>
2021-08-25 12:18:38 -0400

    updated find_packages() to explicitly include only the pygeo package

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author_email="",
     url="https://github.com/mdolab/pygeo",
     license="Apache License Version 2.0",
-    packages=find_packages(),
+    packages=find_packages(include=["pygeo*"]),
     install_requires=["numpy>=1.16", "pyspline>=1.1", "scipy>=1.2", "mpi4py>=3.0", "mdolab-baseclasses"],
     extras_require={"testing": ["numpy-stl", "parameterized"]},
     classifiers=["Operating System :: OS Independent", "Programming Language :: Python"],
