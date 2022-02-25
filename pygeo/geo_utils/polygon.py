aa0ff231bc5833255eeaa6a3bf560ade3ae670b9
Neil Wu <602725+nwu63@users.noreply.github.com>
2022-02-25 18:17:08 -0500

    moved more geometry code outside of classes

--- pygeo/geo_utils/polygon.py
+++ pygeo/geo_utils/polygon.py
@@ -6,10 +6,13 @@ import numpy as np
 
 
 def areaPoly(nodes):
-    # Return the area of the polygon. Note that the input need not be
-    # strictly a polygon, (closed curve in 2 dimensions.) The approach
-    # we take here is to find the centroid, then sum the area of the
-    # 3d triangles. THIS APPROACH ONLY WORKS FOR CONVEX POLYGONS!
+    """Return the area of the polygon.
+    Note that the input need not be strictly a polygon (closed curve in 2 dimensions).
+    The approach we take here is to find the centroid, then sum the area of the
+    3d triangles.
+
+    .. warning:: This approach only works for convex polygons
+    """
 
     c = np.average(nodes, axis=0)
     area = 0.0
@@ -22,10 +25,13 @@ def areaPoly(nodes):
 
 
 def volumePoly(lowerNodes, upperNodes):
-    # Compute the volume of a 'polyhedreon' defined by a loop of nodes
-    # on the 'bottom' and a loop on the 'top'. Like areaPoly, we use
-    # the centroid to split the polygon into triangles. THIS ONLY
-    # WORKS FOR CONVEX POLYGONS
+    """
+    Compute the volume of a 'polyhedron' defined by a loop of nodes
+    on the 'bottom' and a loop on the 'top'.
+    Like areaPoly, we use the centroid to split the polygon into triangles.
+
+    .. warning:: This only works for convex polygons
+    """
 
     lc = np.average(lowerNodes, axis=0)
     uc = np.average(upperNodes, axis=0)
@@ -35,8 +41,7 @@ def volumePoly(lowerNodes, upperNodes):
     for ii in range(len(lowerNodes)):
         # Each "wedge" can be sub-divided to 3 tetrahedra
 
-        # The following indices define the decomposition into three
-        # tetra hedrea
+        # The following indices define the decomposition into three tetrahedra
 
         # 3 5 4 1
         # 5 2 1 0
@@ -75,3 +80,201 @@ def volTetra(nodes):
     V = (1.0 / 6.0) * np.linalg.norm(np.dot(a, np.cross(b, c)))
 
     return V
+
+
+def volpym(a, b, c, d, p):
+    """
+    Compute volume of a square-based pyramid
+    """
+    fourth = 1.0 / 4.0
+
+    volume = (
+        (p[0] - fourth * (a[0] + b[0] + c[0] + d[0])) * ((a[1] - c[1]) * (b[2] - d[2]) - (a[2] - c[2]) * (b[1] - d[1]))
+        + (p[1] - fourth * (a[1] + b[1] + c[1] + d[1]))
+        * ((a[2] - c[2]) * (b[0] - d[0]) - (a[0] - c[0]) * (b[2] - d[2]))
+        + (p[2] - fourth * (a[2] + b[2] + c[2] + d[2]))
+        * ((a[0] - c[0]) * (b[1] - d[1]) - (a[1] - c[1]) * (b[0] - d[0]))
+    )
+
+    return volume
+
+
+def volpym_b(a, b, c, d, p, ab, bb, cb, db, pb):
+    """
+    Compute the reverse-mode derivative of the square-based
+    pyramid. This has been copied from reverse-mode AD'ed tapenade
+    fortran code and converted to python to use vectors for the
+    points.
+    """
+    fourth = 1.0 / 4.0
+    volpymb = 1.0
+    tempb = ((a[1] - c[1]) * (b[2] - d[2]) - (a[2] - c[2]) * (b[1] - d[1])) * volpymb
+    tempb0 = -(fourth * tempb)
+    tempb1 = (p[0] - fourth * (a[0] + b[0] + c[0] + d[0])) * volpymb
+    tempb2 = (b[2] - d[2]) * tempb1
+    tempb3 = (a[1] - c[1]) * tempb1
+    tempb4 = -((b[1] - d[1]) * tempb1)
+    tempb5 = -((a[2] - c[2]) * tempb1)
+    tempb6 = ((a[2] - c[2]) * (b[0] - d[0]) - (a[0] - c[0]) * (b[2] - d[2])) * volpymb
+    tempb7 = -(fourth * tempb6)
+    tempb8 = (p[1] - fourth * (a[1] + b[1] + c[1] + d[1])) * volpymb
+    tempb9 = (b[0] - d[0]) * tempb8
+    tempb10 = (a[2] - c[2]) * tempb8
+    tempb11 = -((b[2] - d[2]) * tempb8)
+    tempb12 = -((a[0] - c[0]) * tempb8)
+    tempb13 = ((a[0] - c[0]) * (b[1] - d[1]) - (a[1] - c[1]) * (b[0] - d[0])) * volpymb
+    tempb14 = -(fourth * tempb13)
+    tempb15 = (p[2] - fourth * (a[2] + b[2] + c[2] + d[2])) * volpymb
+    tempb16 = (b[1] - d[1]) * tempb15
+    tempb17 = (a[0] - c[0]) * tempb15
+    tempb18 = -((b[0] - d[0]) * tempb15)
+    tempb19 = -((a[1] - c[1]) * tempb15)
+    pb[0] = pb[0] + tempb
+    ab[0] = ab[0] + tempb16 + tempb11 + tempb0
+    bb[0] = bb[0] + tempb19 + tempb10 + tempb0
+    cb[0] = cb[0] + tempb0 - tempb11 - tempb16
+    db[0] = db[0] + tempb0 - tempb10 - tempb19
+    ab[1] = ab[1] + tempb18 + tempb7 + tempb2
+    cb[1] = cb[1] + tempb7 - tempb18 - tempb2
+    bb[2] = bb[2] + tempb14 + tempb12 + tempb3
+    db[2] = db[2] + tempb14 - tempb12 - tempb3
+    ab[2] = ab[2] + tempb14 + tempb9 + tempb4
+    cb[2] = cb[2] + tempb14 - tempb9 - tempb4
+    bb[1] = bb[1] + tempb17 + tempb7 + tempb5
+    db[1] = db[1] + tempb7 - tempb17 - tempb5
+    pb[1] = pb[1] + tempb6
+    pb[2] = pb[2] + tempb13
+
+
+def evalVolumeHex(x0, x1, x2, x3, x4, x5, x6, x7):
+    """
+    Evaluate the volume of the hexahedral volume defined by the
+    the 8 corners.
+
+    Parameters
+    ----------
+    x{0:7} : arrays or size (3)
+        Array of defining the coordinates of the volume
+    """
+
+    p = np.average([x0, x1, x2, x3, x4, x5, x6, x7], axis=0)
+    V = 0.0
+    V += volpym(x0, x1, x3, x2, p)
+    V += volpym(x0, x2, x6, x4, p)
+    V += volpym(x0, x4, x5, x1, p)
+    V += volpym(x1, x5, x7, x3, p)
+    V += volpym(x2, x3, x7, x6, p)
+    V += volpym(x4, x6, x7, x5, p)
+    V /= 6.0
+
+    return V
+
+
+def evalVolumeHex_b(x0, x1, x2, x3, x4, x5, x6, x7, x0b, x1b, x2b, x3b, x4b, x5b, x6b, x7b):
+    """
+    Evaluate the derivative of the volume defined by the 8
+    coordinates in the array x.
+
+    Parameters
+    ----------
+    x{0:7} : arrays of len 3
+        Arrays of defining the coordinates of the volume
+
+    Returns
+    -------
+    xb{0:7} : arrays of len 3
+        Derivatives of the volume wrt the points.
+    """
+
+    p = np.average([x0, x1, x2, x3, x4, x5, x6, x7], axis=0)
+    pb = np.zeros(3)
+    volpym_b(x0, x1, x3, x2, p, x0b, x1b, x3b, x2b, pb)
+    volpym_b(x0, x2, x6, x4, p, x0b, x2b, x6b, x4b, pb)
+    volpym_b(x0, x4, x5, x1, p, x0b, x4b, x5b, x1b, pb)
+    volpym_b(x1, x5, x7, x3, p, x1b, x5b, x7b, x3b, pb)
+    volpym_b(x2, x3, x7, x6, p, x2b, x3b, x7b, x6b, pb)
+    volpym_b(x4, x6, x7, x5, p, x4b, x6b, x7b, x5b, pb)
+
+    pb /= 8.0
+    x0b += pb
+    x1b += pb
+    x2b += pb
+    x3b += pb
+    x4b += pb
+    x5b += pb
+    x6b += pb
+    x7b += pb
+
+
+def compute_triangulated_volume(p0, p1, p2):
+    """
+    Compute the volume of a triangulated volume by computing
+    the signed areas.
+
+    Parameters
+    ----------
+    p0, p1, p2 : arrays
+        Coordinates of the vertices of the triangulated mesh
+
+    Returns
+    -------
+    volume : float
+        The volume of the triangulated surface
+
+    References
+    ----------
+    The method is described in, among other places,
+    EFFICIENT FEATURE EXTRACTION FOR 2D/3D OBJECTS IN MESH REPRESENTATION
+    by Cha Zhang and Tsuhan Chen,
+    http://chenlab.ece.cornell.edu/Publication/Cha/icip01_Cha.pdf
+    """
+
+    volume = (
+        np.sum(
+            p1[:, 0] * p2[:, 1] * p0[:, 2]
+            + p2[:, 0] * p0[:, 1] * p1[:, 2]
+            + p0[:, 0] * p1[:, 1] * p2[:, 2]
+            - p1[:, 0] * p0[:, 1] * p2[:, 2]
+            - p2[:, 0] * p1[:, 1] * p0[:, 2]
+            - p0[:, 0] * p2[:, 1] * p1[:, 2]
+        )
+        / 6.0
+    )
+
+    return volume
+
+
+def compute_triangulated_volume_b(p0, p1, p2):
+    """
+    Compute the gradients of the volume with respect to
+    the mesh vertices.
+
+    Parameters
+    ----------
+    p0, p1, p2 : arrays
+        Coordinates of the vertices of the triangulated mesh
+
+    Returns
+    -------
+    grad_0, grad_1, grad_2 : arrays
+        Gradients of volume with respect to vertex coordinates
+    """
+
+    num_pts = p0.shape[0]
+    grad_0 = np.zeros((num_pts, 3))
+    grad_1 = np.zeros((num_pts, 3))
+    grad_2 = np.zeros((num_pts, 3))
+
+    grad_0[:, 0] = p1[:, 1] * p2[:, 2] - p1[:, 2] * p2[:, 1]
+    grad_0[:, 1] = p1[:, 2] * p2[:, 0] - p1[:, 0] * p2[:, 2]
+    grad_0[:, 2] = p1[:, 0] * p2[:, 1] - p1[:, 1] * p2[:, 0]
+
+    grad_1[:, 0] = p0[:, 2] * p2[:, 1] - p0[:, 1] * p2[:, 2]
+    grad_1[:, 1] = p0[:, 0] * p2[:, 2] - p0[:, 2] * p2[:, 0]
+    grad_1[:, 2] = p0[:, 1] * p2[:, 0] - p0[:, 0] * p2[:, 1]
+
+    grad_2[:, 0] = p0[:, 1] * p1[:, 2] - p0[:, 2] * p1[:, 1]
+    grad_2[:, 1] = p0[:, 2] * p1[:, 0] - p0[:, 0] * p1[:, 2]
+    grad_2[:, 2] = p0[:, 0] * p1[:, 1] - p0[:, 1] * p1[:, 0]
+
+    return grad_0 / 6.0, grad_1 / 6.0, grad_2 / 6.0
