f24b094b3c637041b2397e9e62f23cd287c923ce
sseraj <sseraj@umich.edu>
2022-04-15 10:41:58 -0400

    moved write_wing_FFD_file to a different file

--- pygeo/geo_utils/__init__.py
+++ pygeo/geo_utils/__init__.py
@@ -7,6 +7,7 @@
 from .bilinear_map import *  # noqa: F401, F403
 from .dcel import *  # noqa: F401, F403
 from .file_io import *  # noqa: F401, F403
+from .ffd_generation import *  # noqa: F401, F403
 from .index_position import *  # noqa: F401, F403
 from .knotvector import *  # noqa: F401, F403
 from .misc import *  # noqa: F401, F403
