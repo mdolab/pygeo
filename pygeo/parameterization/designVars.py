79afd535ba4ae67a37c1fb8433ee97838f6e0cf8
arshsaja <arshsaja@umich.edu>
2022-09-09 14:58:31 -0400

    Running black and flake8

--- pygeo/parameterization/designVars.py
+++ pygeo/parameterization/designVars.py
@@ -319,7 +319,9 @@ class vspDV(geoDV):
         """
         Internal class for storing VSP design variable information
         """
-        super().__init__(name=dvName, value=np.atleast_1d(np.array(value)), nVal=1, lower=lower, upper=upper, scale=scale)
+        super().__init__(
+            name=dvName, value=np.atleast_1d(np.array(value)), nVal=1, lower=lower, upper=upper, scale=scale
+        )
         self.parmID = parmID
         self.component = component
         self.group = group
