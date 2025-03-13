90a9626319592defaf86d0a52e016384644fa1e5
Alasdair Gray <alachris@umich.edu>
2025-03-13 19:24:36 -0400

    isort

--- tests/reg_tests/test_ffdGeneration.py
+++ tests/reg_tests/test_ffdGeneration.py
@@ -8,7 +8,8 @@ from stl.mesh import Mesh
 
 # First party modules
 from pygeo import DVGeometry
-from pygeo.geo_utils import createFittedWingFFD, write_wing_FFD_file, createMidsurfaceMesh
+from pygeo.geo_utils import (createFittedWingFFD, createMidsurfaceMesh,
+                             write_wing_FFD_file)
 
 baseDir = os.path.dirname(os.path.abspath(__file__))
 
