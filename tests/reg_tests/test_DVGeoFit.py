3e6b978f1c2cf3bef368c13ad02a83466da674b4
Alasdair Gray <alachris@umich.edu>
2025-08-19 14:31:09 -0400

    New test

--- tests/reg_tests/test_DVGeoFit.py
+++ tests/reg_tests/test_DVGeoFit.py
@@ -15,14 +15,16 @@ import unittest
 # External modules
 import numpy as np
 from stl import mesh
+from mpi4py import MPI
 
 # First party modules
 from pygeo import DVGeometry
 
 
 class TestDVGeoFit(unittest.TestCase):
-    N_PROCS = 1
+    N_PROCS = 2
     ptSetName = "test_pts"
+    comm = MPI.COMM_WORLD
 
     def setUp(self):
         # Store the path where this current script lives
@@ -39,6 +41,14 @@ class TestDVGeoFit(unittest.TestCase):
         self.meshFile = os.path.join(self.input_folder, "c172.stl")
         self.testMesh = mesh.Mesh.from_file(self.meshFile)
         self.pts = self.testMesh.vectors.reshape(-1, 3) / 1000.0
+        # Split the points between the two procs
+        nPts = self.pts.shape[0]
+        numProcs = self.comm.size
+        if numProcs > 1:
+            rank = self.comm.rank
+            startInd = rank * (nPts // numProcs)
+            endInd = max((rank + 1) * (nPts // numProcs), nPts)
+            self.pts = self.pts[startInd:endInd]
 
         self.DVGeoRef = self.setupDVGeo(self.ffdFile, self.pts)
         self.DVGeoFit = self.setupDVGeo(self.ffdFile, self.pts)
@@ -77,9 +87,10 @@ class TestDVGeoFit(unittest.TestCase):
 
         return DVGeo
 
-    def test_c172_perfect_fit(self):
-
-        # Test that if we start from the same parameterization and DVs, the fitting should return the same values
+    def test_c172_perfect_fit_same_dvs(self):
+        """
+        Test that if we start from the same parameterization and DVs, the fitting should return the same values
+        """
 
         self.DVGeoRef.setDesignVars(self.perturbedDVs)
         self.DVGeoFit.setDesignVars(self.perturbedDVs)
@@ -95,24 +106,24 @@ class TestDVGeoFit(unittest.TestCase):
         fit_pts = self.DVGeoFit.update(self.ptSetName)
         np.testing.assert_array_equal(ref_pts, fit_pts)
 
-        # ==============================================================================
-        # Fit the original FFD to the deformed points
-        # ==============================================================================
-        # xDV_fit, result = self.DVGeoFit.fitDVGeo(self.DVGeoRef, self.ptSetName, xtol=1e-6, ftol=1e-6, gtol=1e-4)
+    def test_c172_perfect_fit_diff_dvs(self):
+        """
+        Test that if we start from the same parameterization but with different DVs, the fitting should converge to the
+        same geometry and similar DV values.
+        """
+        self.DVGeoRef.setDesignVars(self.perturbedDVs)
+        fitDVs, result = self.DVGeoFit.fitDVGeo(
+            self.DVGeoRef, self.ptSetName, xtol=1e-6, ftol=1e-6, gtol=1e-5, max_nfev=40
+        )
+        self.assertEqual(result.success, True)
+        # Check that the deformed points are the same
+        ref_pts = self.DVGeoRef.update(self.ptSetName)
+        fit_pts = self.DVGeoFit.update(self.ptSetName)
+        np.testing.assert_allclose(ref_pts, fit_pts, rtol=np.inf, atol=1e-5)
 
-        # # ==============================================================================
-        # # Check the results
-        # # ==============================================================================
-        # # Check that the deformed points are the same
-        # new_pts = DVGeo.update(self.ptSetName)
-        # deformed_pts = DVGeo_deformed.update(self.ptSetName)
-        # fitError = np.linalg.norm(new_pts - deformed_pts, axis=1)
-        # # Make sure all points in the fitted geometry are within 1e-3 of the points they were fit to
-        # np.testing.assert_array_less(fitError, 1e-3 * np.ones_like(fitError))
-
-        # # Check that the design variables are the same
-        # for key in xDV_fit.keys():
-        #     np.testing.assert_allclose(xDV_fit[key], randomDVs[key], rtol=1e-2, atol=1e-2)
+        # Check that the design variables are somewhat similar
+        for key in fitDVs.keys():
+            np.testing.assert_allclose(fitDVs[key], self.perturbedDVs[key], rtol=0.05, atol=0.05)
 
 
 if __name__ == "__main__":
