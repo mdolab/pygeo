657b41236f0eb8796abcfb1098f736bb7eea5eec
Alasdair Gray <alachris@umich.edu>
2026-06-01 14:01:01 -0400

    Remove commented code

--- tests/reg_tests/test_DVGeometry.py
+++ tests/reg_tests/test_DVGeometry.py
@@ -1503,6 +1503,3 @@ class RegTestPyGeo(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-    # import xmlrunner
-    # unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
